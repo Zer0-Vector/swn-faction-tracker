cdb79d33af5c935f18522ff4bd2557d2bd0327e0
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:16:18 -0500

    Simplify hp initialization; additional fields

--- src/types/PurchasedAsset.ts
+++ src/types/PurchasedAsset.ts
@@ -1,24 +1,19 @@
-import ASSETS from "../data/Assets";
-
-export default class PurchasedAsset {
+export class PurchasedAsset {
 
   id: number;
   name: string;
   hp: number;
+  nickname?: string;
+  location?: string;
 
-  constructor(id: number, name: string, hp?: number) {
+  constructor(id: number, name: string, hp: number) {
     this.id = id;
     this.name = name;
-    if (hp) {
-      this.hp = hp;
-    } else {
-      const rawMaxHp = ASSETS[name].maxHp;
-      if (rawMaxHp === null) {
-        throw new Error(`Must specify hp for '${name}'`);
-      } else {
-        this.hp = rawMaxHp;
-      }
-    }
+    this.hp = hp;
+  }
+
+  static getKey(factionName: string, pa: PurchasedAsset): string {
+    return `${factionName}.${pa.name}.${pa.id}`;
   }
 
 }
