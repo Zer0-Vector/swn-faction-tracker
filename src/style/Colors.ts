efaf5fd32a39ca08bf6d1508913f43fa33de18fd
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:16:30 -0500

    move colors to separate file. adjust colors

---
+++ src/style/Colors.ts
@@ -0,0 +1,149 @@
+enum Shade {
+  DARKER = -2,
+  DARK = -1,
+  BASE = 0,
+  LIGHT = 1,
+  LIGHTER = 2,
+}
+
+export const DARKER = Shade.DARKER;
+export const DARK = Shade.DARK;
+export const BASE = Shade.BASE;
+export const LIGHT = Shade.LIGHT;
+export const LIGHTER = Shade.LIGHTER;
+
+type ColorType = {
+  [K in Shade]: string;
+};
+
+type ColorVariant = "pastel" | "very light pale pastel" | "greyish darkest" | "greyish lightest" | "shiny";
+
+interface MainPalette {
+  primary: ColorType;
+  secondary: ColorType;
+  tertiary: ColorType;
+}
+
+type PaletteMap = { [K in ColorVariant]: MainPalette };
+
+// https://paletton.com/#uid=33x0I0kmpJc6c+ygdUgrbuKqJdi
+export const COLORS: PaletteMap = {
+  "pastel": {
+    primary: {
+      [DARKER]: "#0F2D44",
+      [DARK]: "#0F2D44",
+      [BASE]: "#4792CC",
+      [LIGHT]: "#7CBBEC",
+      [LIGHTER]: "#D0EAFE",
+    },
+    secondary: {
+      [DARKER]: "#6A1111",
+      [DARK]: "#F52525",
+      [BASE]: "#FF4C4C",
+      [LIGHT]: "#FF7E7E",
+      [LIGHTER]: "#FFCECE",
+    },
+    tertiary: {
+      [DARKER]: "#6A5B11",
+      [DARK]: "#F5D125",
+      [BASE]: "#FFE04C",
+      [LIGHT]: "#FFE97E",
+      [LIGHTER]: "#FFF7CE",
+    },
+  },
+  "greyish darkest": {
+    // TODO
+    primary: {
+      [DARKER]: "#1C2C38",
+      [DARK]: "#1C2C38",
+      [BASE]: "#161819",
+      [LIGHT]: "#2C3338",
+      [LIGHTER]: "#394853",
+    },
+    secondary: {
+      [DARKER]: "#565229",
+      [DARK]: "#403E2B",
+      [BASE]: "#272722",
+      [LIGHT]: "#585643",
+      [LIGHTER]: "#817D55",
+    },
+    tertiary: {
+      [DARKER]: "#4D2531",
+      [DARK]: "#39272C",
+      [BASE]: "#231F20",
+      [LIGHT]: "#4E3B41",
+      [LIGHTER]: "#744C58",
+    },
+  },
+  "greyish lightest": {
+    // TODO
+    primary: {
+      [DARKER]: "#889096",
+      [DARK]: "#9DA3A9",
+      [BASE]: "#D3D7DA",
+      [LIGHT]: "#A8B7C3",
+      [LIGHTER]: "#889DAE",
+    },
+    secondary: {
+      [DARKER]: "#E9E7D3",
+      [DARK]: "#FFFDEC",
+      [BASE]: "#FFFEF6",
+      [LIGHT]: "#FFFCD9",
+      [LIGHTER]: "#FFFAC4",
+    },
+    tertiary: {
+      [DARKER]: "#D0BCC2",
+      [DARK]: "#E5D4DA",
+      [BASE]: "#F4EBEE",
+      [LIGHT]: "#EDCAD4",
+      [LIGHTER]: "#E7B1C1",
+    },
+  },
+  "very light pale pastel": {
+    // TODO
+    primary: {
+      [DARKER]: "#3C6585",
+      [DARK]: "#577C98",
+      [BASE]: "#7695AD",
+      [LIGHT]: "#9CB4C7",
+      [LIGHTER]: "#C1D1DD",
+    },
+    secondary: {
+      [DARKER]: "#CFC456",
+      [DARK]: "#ECE380",
+      [BASE]: "#FFF7A8",
+      [LIGHT]: "#FFFAC4",
+      [LIGHTER]: "#FFFCDD",
+    },
+    tertiary: {
+      [DARKER]: "#B94D6E",
+      [DARK]: "#D37290",
+      [BASE]: "#E798B0",
+      [LIGHT]: "#EEB7C8",
+      [LIGHTER]: "#F5D4DE",
+    },
+  },
+  "shiny": {
+    primary: {
+      [DARKER]: "#003D6B",
+      [DARK]: "#004F8B",
+      [BASE]: "#1097FF",
+      [LIGHT]: "#79C5FF",
+      [LIGHTER]: "#A9DAFF",
+    },
+    secondary: {
+      [DARKER]: "#A79800",
+      [DARK]: "#D9C500",
+      [BASE]: "#FFE800",
+      [LIGHT]: "#FFF270",
+      [LIGHTER]: "#FFF7A4",
+    },
+    tertiary: {
+      [DARKER]: "#95002D",
+      [DARK]: "#C2003B",
+      [BASE]: "#FF004D",
+      [LIGHT]: "#FF709B",
+      [LIGHTER]: "#FFA4BF",
+    },
+  }
+};
--- src/style/Theme.ts
+++ src/style/Theme.ts
@@ -1,186 +1,39 @@
 import { createTheme } from "@mui/material/styles";
 
-enum Shade {
-  DARKER = -2,
-  DARK = -1,
-  BASE = 0,
-  LIGHT = 1,
-  LIGHTER = 2,
-}
-const DARKER = Shade.DARKER;
-const DARK = Shade.DARK;
-const BASE = Shade.BASE;
-const LIGHT = Shade.LIGHT;
-const LIGHTER = Shade.LIGHTER;
-
-type ColorType = {
-  [K in Shade]: string;
-};
-
-type ColorVariant = "main" | "very light pastel" | "greyish darkest" | "greyish lighter" | "shiny";
-
-interface MainPalette {
-  primary: ColorType;
-  secondary: ColorType;
-  tertiary: ColorType;
-}
-
-type PaletteMap = { [K in ColorVariant]: MainPalette };
-
-// https://paletton.com/#uid=33x0I0kmpJc6c+ygdUgrbuKqJdi
-const COLORS: PaletteMap = {
-  "main": {
-    primary: {
-      [DARKER]: "#0F2D44",
-      [DARK]: "#0F2D44",
-      [BASE]: "#4792CC",
-      [LIGHT]: "#7CBBEC",
-      [LIGHTER]: "#D0EAFE",
-    },
-    secondary: {
-      [DARKER]: "#6A1111",
-      [DARK]: "#F52525",
-      [BASE]: "#FF4C4C",
-      [LIGHT]: "#FF7E7E",
-      [LIGHTER]: "#FFCECE",
-    },
-    tertiary: {
-      [DARKER]: "#6A5B11",
-      [DARK]: "#F5D125",
-      [BASE]: "#FFE04C",
-      [LIGHT]: "#FFE97E",
-      [LIGHTER]: "#FFF7CE",
-    },
-  },
-  "greyish darkest": {
-    // TODO
-    primary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-    secondary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-    tertiary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-  },
-  "greyish lighter": {
-    // TODO
-    primary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-    secondary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-    tertiary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-  },
-  "very light pastel": {
-    // TODO
-    primary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-    secondary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-    tertiary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-  },
-  "shiny": {
-    // TODO
-    primary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-    secondary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-    tertiary: {
-      [DARKER]: "#",
-      [DARK]: "#",
-      [BASE]: "#",
-      [LIGHT]: "#",
-      [LIGHTER]: "#",
-    },
-  }
-};
+import { BASE, COLORS, DARK, DARKER, LIGHT, LIGHTER } from "./Colors";
 
 export const THEME = createTheme({
   palette: {
     mode: 'dark',
     primary: {
-      main: COLORS.main.primary[BASE],
-      light: COLORS.main.primary[LIGHT],
-      dark: COLORS.main.primary[DARK],
-      contrastText: COLORS.main.primary[LIGHTER],
+      main: COLORS.pastel.primary[BASE],
+      light: COLORS.pastel.primary[LIGHT],
+      dark: COLORS.pastel.primary[DARK],
+      contrastText: COLORS.pastel.primary[LIGHTER],
     },
     secondary: {
-      main: COLORS.main.secondary[BASE],
-      light: COLORS.main.secondary[LIGHT],
-      dark: COLORS.main.secondary[DARK],
-      contrastText: COLORS.main.secondary[LIGHTER],
+      main: COLORS.pastel.secondary[BASE],
+      light: COLORS.pastel.secondary[LIGHT],
+      dark: COLORS.pastel.secondary[DARK],
+      contrastText: COLORS.pastel.secondary[LIGHTER],
     },
     tertiary: {
-      main: COLORS.main.tertiary[BASE],
-      light: COLORS.main.tertiary[LIGHT],
-      dark: COLORS.main.tertiary[DARK],
-      contrastText: COLORS.main.tertiary[LIGHTER],
+      main: COLORS.pastel.tertiary[BASE],
+      light: COLORS.pastel.tertiary[LIGHT],
+      dark: COLORS.pastel.tertiary[DARK],
+      contrastText: COLORS.pastel.tertiary[LIGHTER],
     },
     action: {
-      selected: COLORS.main.tertiary[DARKER],
-      dragging: COLORS.main.secondary[DARKER],
+      selected: COLORS.pastel.tertiary[DARKER],
+      dragging: COLORS.pastel.secondary[DARKER],
     },
     background: {
-      default: "#282c34",
-      paper: "#383c44",
-      paper2: "#484c54",
-      paper3: "#50546c",
+      default: COLORS["greyish darkest"].primary[BASE],
+      paper: COLORS["greyish darkest"].primary[LIGHT],
+      paper2: COLORS["greyish darkest"].primary[LIGHTER],
     },
+    text: {
+    }
   },
   typography: {
     body1: {
