9596bb2aa6f4dead1337f68d760a92c32c386eb5
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:16:16 -0500

    Extract ConfirmDialog component

---
+++ src/components/ConfirmDialog/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+
+type ConfirmDialogProps = {
+  title: string,
+  message: string,
+  buttonText: string,
+  open: boolean,
+  onClose: () => void,
+  onConfirm: () => void,
+};
+
+export default function ConfirmDialog({ title, message, buttonText, open, onClose, onConfirm }: ConfirmDialogProps) {
+  const handleConfirm = (evt: React.MouseEvent<HTMLElement>) => {
+    evt.stopPropagation();
+    onConfirm();
+    onClose();
+  };
+
+  const handleCancel = (evt: React.MouseEvent<HTMLElement>) => {
+    evt.stopPropagation();
+    onClose();
+  };
+  
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogContent>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContentText>{message}</DialogContentText>
+        <DialogActions>
+          <Button onClick={handleCancel}>Cancel</Button>
+          <Button onClick={handleConfirm}>{buttonText}</Button>
+        </DialogActions>
+      </DialogContent>
+    </Dialog>
+  );
+}
