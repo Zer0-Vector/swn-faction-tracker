3fde217016b46067886bc247694f3287f131d433
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:16:02 -0500

    Show faction details conditionally; based on selection; Also, some style tweaks

--- src/components/PrimaryPanel/index.tsx
+++ src/components/PrimaryPanel/index.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import FactionList from "../FactionList";
 import FactionActionToolbar from "../FactionActionToolbar";
+import { UiStateContext } from "../../contexts/UiStateContext";
+import { GameContext } from "../../contexts/GameContext";
+import FactionInfo from "../../types/FactionInfo";
+import FactionDetails from "../FactionDetails";
+import AssetList from "../AssetList";
 
 export default function PrimaryPanel() {
+  const { state } = useContext(GameContext);
+  const { state: uiState } = useContext(UiStateContext);
+
+  const faction: FactionInfo | undefined = state.factions[uiState.selectedFaction || "-=NULL=-"];
+
   return (
     <Box sx={{
         display: "flex",
-        minHeight: "calc(100vh - 6rem)",
+        minHeight: "calc(100vh - 4rem)",
         flexDirection: "column",
         alignItems: "stretch",
-        padding: "3rem 0",
+        padding: "2rem 0",
       }}
       data-testid="app-inner-box"
     >
@@ -22,24 +32,29 @@ export default function PrimaryPanel() {
         width: "100%"
       }}>
         <Box sx={{ 
-          width: "40%",
+          flexGrow: 3,
           marginTop: "2rem",
           marginX: "2rem",
           padding: "1rem",
           backgroundColor: "background.paper",
         }}>
-          {/* <AddFactionForm /> */}
           <FactionActionToolbar />
           <FactionList />
         </Box>
-        <Box sx={{
-          flexGrow: "1",
-          marginTop: "2rem",
-          marginX: "2rem",
-          padding: "1rem",
-        }}>
-          <Typography>Test 123</Typography>
-        </Box>
+        {
+          faction ? (
+            <Box sx={{
+              flexGrow: 10,
+              marginTop: "2rem",
+              marginX: "2rem",
+              padding: "1rem",
+            }}>
+              <Typography variant="h2">{faction.name}</Typography>
+              <FactionDetails />
+              <AssetList />
+            </Box>
+          ) : undefined
+        }
       </Box>
     </Box>
   );
