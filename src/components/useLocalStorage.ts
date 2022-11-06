a1a31ca88dced4f0661b0b9fc1e3678254102dd8
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:15:41 -0500

    useLocalStorage hook

---
+++ src/components/useLocalStorage.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function getStorageValue<T>(key: string, defaultValue: T): T {
+  const saved = localStorage.getItem(key);
+  if (saved) {
+    return JSON.parse(saved) as T;
+  } else {
+    return defaultValue;
+  }
+
+}
+
+export function useLocalStorage<T>(key: string, defaultValue: T) {
+  const [value, setValue] = useState(() => getStorageValue(key, defaultValue));
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value])
+
+  return [value, setValue];
+}
