29cfa999019ebef97a39f0cd9a6e6c47a1dca924
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:15:44 -0500

    create EditableNameText component

---
+++ src/components/EditableNameText/index.tsx
@@ -0,0 +1,69 @@
+import { TextField } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+
+type EditableNameTextProps = {
+  children: string,
+  updateValue: (newValue: string) => void,
+  divStyle?: React.CSSProperties
+}
+
+export default function EditableNameText({ children, updateValue, divStyle }: EditableNameTextProps) {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [hasChanged, setHasChanged] = useState<boolean>(false);
+  const textFieldRef = useRef<HTMLInputElement>(null);
+
+  const actualDivStyle: React.CSSProperties = {
+    ...divStyle,
+    textOverflow: "ellipsis",
+    width: "100%",
+    height: "45px",
+  }
+
+  useEffect(() => {
+    if (isEditing) {
+      textFieldRef.current?.select();
+    }
+  }, [isEditing])
+
+  const enterEditMode = () => {
+    setIsEditing(true);
+  }
+
+  const exitEditMode = (evt: React.SyntheticEvent<Element>) => {
+    evt.preventDefault();
+    if (hasChanged) {
+      updateValue(textFieldRef.current?.value as string);
+      setHasChanged(false);
+    }
+    setIsEditing(false);
+  }
+
+  const handleKeyUp = (evt: React.KeyboardEvent<HTMLFormElement>) => {
+    if (evt.key === 'Escape') {
+      setIsEditing(false);
+    }
+  }
+
+  const textChanged = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setHasChanged(true);
+  }
+
+  if (isEditing) {
+    return (
+      <form onSubmit={exitEditMode} onKeyUp={handleKeyUp} style={actualDivStyle}>
+        <TextField
+          inputRef={textFieldRef}
+          onBlur={exitEditMode}
+          onInput={textChanged}
+          defaultValue={children}
+          fullWidth={true}
+          inputProps={{ style: { height: "100%" } }}
+        />
+      </form>
+    ) 
+  } else {
+    return (
+      <div style={actualDivStyle} onDoubleClick={enterEditMode}>{children}</div>
+    );
+  }
+}
