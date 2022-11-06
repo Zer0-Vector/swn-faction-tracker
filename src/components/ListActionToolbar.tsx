8206649f73cf174a15f72014ce03de7c5b469f8f
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:16:24 -0500

    tweak spacing in ListActionToolbar. use theme for spacing

--- src/components/ListActionToolbar.tsx
+++ src/components/ListActionToolbar.tsx
@@ -13,23 +13,30 @@ interface ListActionToolbarProps {
 
 export default function ListActionToolbar({ removable, onAddClick, onRemoveClick, children }: ListActionToolbarProps) {
   return (
-    <Box sx={{ paddingBottom: "1rem", display: "flex", flexDirection: "row", gap: "1rem" }}>
-      <Fab
-        size="medium"
-        color="primary"
-        onClick={onAddClick}
-      >
-        <AddIcon />
-      </Fab>
-      <Fab
-        size="medium"
-        color="primary"
-        disabled={!removable}
-        onClick={onRemoveClick}
-      >
-        <RemoveIcon />
-      </Fab>
+    <>
+      <Box sx={theme => ({ 
+        paddingBottom: "1rem",
+        display: "flex",
+        flexDirection: "row",
+        gap: theme.spacing(2),
+      })}>
+        <Fab
+          size="medium"
+          color="primary"
+          onClick={onAddClick}
+        >
+          <AddIcon />
+        </Fab>
+        <Fab
+          size="medium"
+          color="primary"
+          disabled={!removable}
+          onClick={onRemoveClick}
+        >
+          <RemoveIcon />
+        </Fab>
+      </Box>
       {children}
-    </Box>
+    </>
   );
 }
