63ab35154014d5fb283923898d52d4f4de60b2b9
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:16:21 -0500

    Rework app header; add tabs.

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { Link, useLocation } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Container from "@mui/material/Container";
+import Toolbar from "@mui/material/Toolbar";
 
 export type PageContainerProps = {
   ref?: React.Ref<unknown>,
@@ -8,18 +14,39 @@ export type PageContainerProps = {
 };
 
 export default function PageContainer({ ref, children }: PageContainerProps) {
+  const location = useLocation();
   return (
     <Box sx={{
         display: "flex",
-        minHeight: "calc(100vh - 4rem)",
+        minHeight: "100vh",
         flexDirection: "column",
         alignItems: "stretch",
-        padding: "2rem 0",
       }}
       ref={ref}
       data-testid="page-container"
     >
-      <Typography variant="h1" sx={{ textAlign: "center" }}>SWN Faction Tracker</Typography>
+      <AppBar>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography variant="h1" sx={{ mr: 3 }}>SWN Faction Tracker</Typography>
+            <Tabs value={location.pathname}>
+              <Tab
+                value="/"
+                label="Factions"
+                component={Link}
+                to="/"
+              />
+              <Tab
+                value="/locations"
+                label="Locations"
+                component={Link}
+                to="/locations"
+              />
+            </Tabs>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Toolbar id="appbar-shim" />
       <Box sx={{
         display: "flex",
         flexDirection: "row",
