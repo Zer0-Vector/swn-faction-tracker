2eeaa9bfbfe7f093825ffae387d5489cc27fc211
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:16:29 -0500

    remove unused color type

--- src/mui-types.ts
+++ src/mui-types.ts
@@ -3,7 +3,6 @@ import "@mui/material/styles/createPalette";
 declare module '@mui/material/styles/createPalette' {
   interface TypeBackground {
     paper2: string;
-    paper3: string;
   }
 
   interface TypeAction {
