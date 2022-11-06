792cf0f67b1ecbd32d243ba9ca653c0d12412f14
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:15:56 -0500

    Utilize StatText in HealthDisplay

--- src/components/HealthDisplay/index.tsx
+++ src/components/HealthDisplay/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import EditableStatText from "../EditableStatText";
-import StatText from "../StatText";
 import Tooltip from "@mui/material/Tooltip";
 import Box from "@mui/material/Box";
 import LinearProgress from "@mui/material/LinearProgress";
+import StatText from "../StatText";
 
 type HealthDisplayProps = {
   onHpUpdate: (hp: string) => void,
@@ -16,12 +16,16 @@ export default function HealthDisplay({ current, max, onHpUpdate }: HealthDispla
     <>
       <Tooltip
         title={
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <EditableStatText updateValue={onHpUpdate}>
+          <Box sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+            <EditableStatText updateValue={onHpUpdate} sx={{ fontSize: "3rem" }} inputSx={{ maxWidth: "4.5em"}}>
               {current.toString()}
             </EditableStatText>
-            /
-            <StatText>
+            <StatText sx={{ fontSize: "3rem" }}>/</StatText>
+            <StatText sx={{ fontSize: "3rem" }}>
               {max}
             </StatText>
           </Box>
@@ -33,9 +37,9 @@ export default function HealthDisplay({ current, max, onHpUpdate }: HealthDispla
           value={100 * current / max}
           variant="determinate"
           sx={{ 
-            width: "7rem",
-            minWidth: "5rem",
-            height: "35%",
+            width: "5rem",
+            minWidth: "2rem",
+            height: "1.25rem"
           }}
         />
       </Tooltip>
