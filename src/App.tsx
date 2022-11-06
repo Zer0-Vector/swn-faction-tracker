1ece2ee2c2e3acd7bb800d35af078dda36fa6a99
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:16:20 -0500

    Add react router and LocationsPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { GameContext } from './contexts/GameContext';
 import { useLocalStorage } from './hooks/useLocalStorage';
 import { GameController, IGameController } from './controllers/GameController';
@@ -11,6 +11,8 @@ import Box from '@mui/material/Box';
 import { createTheme } from '@mui/material/styles';
 import PrimaryPanel from './components/PrimaryPanel';
 import RuntimeGameState from './types/RuntimeGameState';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LocationsPanel from './components/LocationsPanel';
 
 function App() {
   const [storedState, setStoredState] = useLocalStorage<StoredGameState>("Faction-GameState", 
@@ -92,15 +94,24 @@ function App() {
     <ThemeProvider theme={theme}>
       <GameContext.Provider value={{state: gameState, controller: gameController}}>
         <UiStateContext.Provider value={{ state: uiState, controller: uiController }}>
-          <Box
-            sx={{
-              color: theme.palette.text.primary,
-              backgroundColor: theme.palette.background.default,
-            }}
-            data-testid="app-root"
-          >
-            <PrimaryPanel />
-          </Box>
+          <Router>
+            <Box
+              sx={{
+                color: theme.palette.text.primary,
+                backgroundColor: theme.palette.background.default,
+              }}
+              data-testid="app-root"
+            >
+                <Routes>
+                  <Route path="/">
+                    <PrimaryPanel />
+                  </Route>
+                  <Route path="/locations">
+                    <LocationsPanel />
+                  </Route>
+                </Routes>
+            </Box>
+          </Router>
         </UiStateContext.Provider>
       </GameContext.Provider>
     </ThemeProvider>
