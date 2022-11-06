1a52508b7123329ed3af37be5a4cea1557d7f8ab
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:16:19 -0500

    remove unused components

--- src/types/GameState.ts
+++
@@ -1,9 +0,0 @@
-import FactionInfo from "./FactionInfo";
-import PurchasedAsset from "./PurchasedAsset";
-
-export default interface GameState {
-  isLoading: boolean;
-  factions: { [id: string]: FactionInfo };
-  factionOrder: string[];
-  assets: { [factionId: string]: PurchasedAsset[] };
-}
