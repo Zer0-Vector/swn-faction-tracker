e2663c594a794bd57fe64b668dfadc2f22f6c3d1
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-06 08:16:19 -0500

    Add AssetDetails to AssetList

--- src/components/AssetList/index.tsx
+++ src/components/AssetList/index.tsx
@@ -1,31 +1,46 @@
 import React, { useContext } from "react";
 import Typography from "@mui/material/Typography";
 import Accordion from "@mui/material/Accordion";
-import Box from "@mui/material/Box";
-import { AccordionSummary } from "@mui/material";
 import { GameContext } from "../../contexts/GameContext";
 import { UiStateContext } from "../../contexts/UiStateContext";
-import PurchasedAsset from "../../types/PurchasedAsset";
+import { PurchasedAsset } from "../../types/PurchasedAsset";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AssetDetails from "../AssetDetails";
 
 export default function AssetList() {
   const { state } = useContext(GameContext);
-  const { state: uiState } = useContext(UiStateContext);
+  const { state: uiState, controller: uiController } = useContext(UiStateContext);
 
-  const assets: PurchasedAsset[] | undefined = state.assets[uiState.selectedFaction || "-=NULL=-"];
+  const assets: PurchasedAsset[] = state.getAssets(uiState.selectedFaction);
   
   console.log("rendering asset list: ", assets);
 
+  const handleSelectAsset = (pa: PurchasedAsset) => (_evt: React.SyntheticEvent, expanded: boolean) => {
+    if (expanded) {
+      uiController.selectAsset(pa);
+    } else {
+      uiController.selectAsset(null);
+    }
+  };
+
   return (
+    // TODO make drag and drop
     <>
       {
-        assets && assets.length > 0 ? (
-          assets.map((pa, index) => (
-            <Accordion key={index}>
-              <AccordionSummary>{pa.info.name}</AccordionSummary>
-            </Accordion>
-          ))
+        assets.length > 0 ? (
+          assets.map((pa, index) => {
+            const expanded = PurchasedAsset.getKey(uiState.selectedFaction as string, pa) === uiState.selectedAssetKey;
+            const name = pa.nickname ? `${pa.nickname} (${pa.name})` : pa.name;
+            return (
+              <Accordion key={index} expanded={expanded} onChange={handleSelectAsset(pa)}>
+                <AccordionSummary sx={{ backgroundColor: expanded ? "primary.main" : "inherit" }}>{name}</AccordionSummary>
+                <AccordionDetails><AssetDetails asset={pa} /></AccordionDetails>
+              </Accordion>
+            );
+          })
         ) : (
-          <i>No Assets</i>
+          <Typography color="warning.main" fontStyle="italic">No Assets</Typography>
         )
       }
     </>
