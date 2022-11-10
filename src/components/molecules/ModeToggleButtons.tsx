bcb4ebc7548a13d7ea3341ce6c0240e63275b955
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-10 07:37:17 -0500

    Create ModeToggleButtons

---
+++ src/components/molecules/ModeToggleButtons.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+
+import { GameContext } from "../../contexts/GameContext";
+
+export default function ModeToggleButtons() {
+  const { state, controller } = useContext(GameContext);
+
+  const handleChange = (_: React.MouseEvent, value: string) => {
+    controller.setMode(value);
+  };
+
+  return (
+    <ToggleButtonGroup
+      value={state.mode}
+      exclusive={true}
+      color="tertiary"
+      onChange={handleChange}
+    >
+      <ToggleButton value="VIEW" fullWidth={true}>View</ToggleButton>
+      <ToggleButton value="EDIT" fullWidth={true}>Free Edit</ToggleButton>
+      <ToggleButton value="TURN" fullWidth={true}>Take Turn</ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
