73fe7096078f45b3b3e9d24c14537137945b5f9e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-10 01:15:10 -0500

    try plantuml args

--- .github/actions/plantuml/entrypoint.sh
+++ .github/actions/plantuml/entrypoint.sh
@@ -5,6 +5,8 @@ echo "time=$time" >> $GITHUB_OUTPUT
 
 echo "args: $@"
 
-ls -l /github/workspace
+ls -l "/github/workspace/$1"
 
-java -jar /opt/plantuml.jar "$@"
+java -jar /opt/plantuml.jar -v -o "/github/workspace/$2" "/github/workspace/$1"
+
+ls -l "/github/workspace/$2"
