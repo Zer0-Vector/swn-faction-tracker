67fd49ea161179c0ebc92000f5c0bf4d2fdb93dd
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-10 01:25:21 -0500

    replace diagrams with custom action

--- .github/actions/plantuml/entrypoint.sh
+++ .github/actions/plantuml/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env sh
 
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
-
 echo "args: $@"
 
-ls -l "/github/workspace/$1"
-
 java -jar /opt/plantuml.jar -v "-t$3" -o "/github/workspace/$2" "/github/workspace/$1"
-
-ls -l "/github/workspace/$2"
