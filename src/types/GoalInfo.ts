3fb7c7d005850fc90b8774700e54d54fedff2864
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-10 15:16:34 -0500

    define GoalInfo

---
+++ src/types/GoalInfo.ts
@@ -0,0 +1,11 @@
+import GoalType from "./GoalType";
+
+type GoalUnit = "Asset" | "Planet" | "Base" | "HP" | "Turn" | "Faction" | "FacCred";
+
+export default interface GoalInfo {
+  type: GoalType;
+  tally: number;
+  target: number;
+  unit: GoalUnit;
+  reward: number;
+}
