4eee1956a71f7ce4b0208196a610b60554615667
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-11 18:00:14 -0500

    stub in tests for FactionHpSummary

---
+++ src/components/molecules/__tests__/FactionHpSummary.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+describe('<FactionHpSummary />', () => {
+  it.todo('renders default value when faction does not exist');
+  it.todo('renders hp and maxHp when faction exists');
+  it.todo('renders TextField after double click on hp');
+  it.todo('does not accept invalid hp values');
+  it.todo('updates hp on Enter and valid value');
+});
