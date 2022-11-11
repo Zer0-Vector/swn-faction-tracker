6ab68f4430ef1c8a0dbff730fe9d4e1985857c5a
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-11 17:55:54 -0500

    stub in ConfirmDialog tests

---
+++ src/components/molecules/__tests__/ConfirmDialog.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+describe('default ConfirmDialog', () => {
+  it.todo('does not render when open=false');
+  it.todo('renders when open=true');
+  it.todo('renders given title');
+  it.todo('renders given message');
+  it.todo('renders given button text');
+  it.todo('renders buttons');
+  it.todo('calls onClose when cancelled');
+  it.todo('calls onConfirm when confirmed');
+});
