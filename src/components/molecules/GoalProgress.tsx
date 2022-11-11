dc7477f3d4934341b2164e9e608c5aa94f2b7295
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-11 09:30:13 -0500

    create GoalProgress component

---
+++ src/components/molecules/GoalProgress.tsx
@@ -0,0 +1,53 @@
+import React, { useContext } from "react";
+
+import { GameContext } from "../../contexts/GameContext";
+import FactionInfo from "../../types/FactionInfo";
+import EditableStatText from "../atoms/EditableStatText";
+import StatText from "../atoms/StatText";
+
+interface GoalProgressProps {
+  faction: FactionInfo;
+}
+
+export default function GoalProgress({ faction }: GoalProgressProps) {
+  const { controller } = useContext(GameContext);
+  
+  if (!faction.goal) {
+    return (
+      <em>Select Goal</em>
+    );
+  }
+
+  const handleUpdateTally = (val: string) => {
+    if (faction.goal) {
+      console.debug(`setting tally: '${val}'`);
+      controller.setGoal(faction.name, {
+        ...faction.goal,
+        tally: parseInt(val),
+      });
+    }
+  };
+
+  const handleUpdateTarget = (val: string) => {
+    if (faction.goal) {
+      controller.setGoal(faction.name, {
+        ...faction.goal,
+        target: parseInt(val),
+      });
+    }
+  };
+
+  const valueOrDefault = (val: number | undefined) => {
+    return val === undefined || val === null ? "??" : val;
+  };
+
+  const { tally, target } = faction.goal;
+
+  return (
+    <span>
+      <EditableStatText updateValue={handleUpdateTally} inputSx={{ width: "3em" }}>{valueOrDefault(tally)}</EditableStatText>
+      <StatText>/</StatText>
+      <EditableStatText updateValue={handleUpdateTarget} inputSx={{ width: "3em" }}>{valueOrDefault(target)}</EditableStatText>
+    </span>
+  );
+}
