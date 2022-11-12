58acedbc34a6ca7d6e1c0544fc80e96cbb44a8c6
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-12 08:09:37 -0500

    create LoginState

---
+++ src/types/LoginState.ts
@@ -0,0 +1,16 @@
+const LoginStates = [
+  "LOGGED_OUT", // before login, or after logout
+  "LOGGING_IN", // login dialog showing
+  "LOGGING_OUT", // logout confirmation showing
+  "LOGGED_IN", // after login dialog success
+  "LOGIN_ERROR", // after login failure, error dialog showing
+  "LOGIN_WAITING", // after login button click, before login success/failure
+  "REGISTERING", // showing registration dialog
+  "REGISTER_WAITING", // after register button click, before register success/failure
+  "REGISTER_ERROR", // after register failure
+  "REGISTERED", // showing instructional dialog, after register success
+] as const;
+
+type LoginState = typeof LoginStates[number];
+
+export default LoginState;
