0d240a3934b3d6bc8bee3070aafb499388d7c4fd
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-14 06:37:08 -0500

    update LoginDialog to utilize firebase

--- src/components/molecules/LoginDialog.tsx
+++ src/components/molecules/LoginDialog.tsx
@@ -10,6 +10,7 @@ import DialogTitle from "@mui/material/DialogTitle";
 import TextField from "@mui/material/TextField";
 
 import { UiStateContext } from "../../contexts/UiStateContext";
+import { FirebaseAuth } from "../../firebase-init";
 import Nullable from "../../types/Nullable";
 
 interface ValidationInfo {
@@ -21,12 +22,12 @@ export default function LoginDialog() {
   const { state: uiState, controller: uiController } = useContext(UiStateContext);
   const [usernameValid, setUsernameValid] = useState<ValidationInfo>({hasChanged: false, valid: false});
   const [passwordValid, setPasswordValid] = useState({hasChanged: false, valid: false});
-  const usernameRef = useRef<Nullable<HTMLInputElement>>(null);
+  const emailRef = useRef<Nullable<HTMLInputElement>>(null);
   const passwordRef = useRef<Nullable<HTMLInputElement>>(null);
 
   const handleClearForm = () => {
-    if (usernameRef.current) {
-      usernameRef.current.value = "";
+    if (emailRef.current) {
+      emailRef.current.value = "";
     }
     if (passwordRef.current) {
       passwordRef.current.value = "";
@@ -46,8 +47,28 @@ export default function LoginDialog() {
     handleClearForm();
   };
 
-  const handleLogin = () => {
+  const handleLogin: React.FormEventHandler<HTMLFormElement> = (evt) => {
+    evt.preventDefault();
+    const username = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    
+    if (!username || !password) {
+      console.error("Username or password not set: ", username, !!password);
+      uiController.setLoginState("LOGGED_OUT");
+      return;
+    }
+
     uiController.setLoginState("LOGIN_WAITING");
+    FirebaseAuth.signInWithEmailAndPassword(
+      username,
+      password
+    ).then(cred => {
+      console.info("Logged in as ", cred.user?.email);
+      uiController.setLoginState("LOGGED_IN");
+    }).catch(reason => {
+      console.error("Error logging in: ", reason);
+      uiController.setLoginState("LOGGED_OUT");
+    });
     handleClearForm();
   };
 
@@ -73,20 +94,23 @@ export default function LoginDialog() {
       || !passwordValid.hasChanged
       || !passwordValid.valid;
 
+  console.debug("Rendering LoginDialog...");
+
   return (
     <Dialog open={uiState.loginState === "LOGGING_IN"}>
       <DialogTitle>Login</DialogTitle>
-      <form onSubmit={handleLogin} action=".">
+      <form onSubmit={handleLogin}>
         <DialogContent>
           <DialogContentText sx={{ my: 2 }}>Enter your credientials.</DialogContentText>
           <Container disableGutters sx={{ display: "grid", gridTemplateColumns: "1fr", gap: 2}}>
             <TextField
-              id="username"
-              label="Username"
+              id="email"
+              label="Email"
               error={usernameError}
               onChange={handleUsernameChange}
               autoFocus={true}
-              autoComplete="username"
+              autoComplete="email"
+              inputRef={emailRef}
             />
             <TextField
               id="password"
@@ -95,6 +119,7 @@ export default function LoginDialog() {
               error={passwordError}
               onChange={handlePasswordChange}
               autoComplete="current-password"
+              inputRef={passwordRef}
             />
           </Container>
         </DialogContent>
