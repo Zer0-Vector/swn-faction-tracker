796fe40aacab53644b09dacc4381eb72fcbd92ef
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-15 08:37:56 -0500

    remove unused states

--- src/types/LoginState.ts
+++ src/types/LoginState.ts
@@ -1,16 +1,16 @@
 const LoginStates = [
   "LOGGED_OUT", // before login, or after logout
-  "LOGGING_IN", // login dialog showing
+  "LOGGING_IN", // login dialog showing (maybe error message)
   "LOGGING_OUT", // logout confirmation showing
   "LOGOUT_WAITING", // after logout confirm, before server response
   "LOGGED_IN", // after login dialog success
-  "LOGIN_ERROR", // after login failure, error dialog showing
   "LOGIN_WAITING", // after login button click, before login success/failure
-  "REGISTERING", // showing registration dialog
+  "REGISTERING", // showing registration dialog (maybe error message)
   "REGISTER_WAITING", // after register button click, before register success/failure
-  "REGISTER_ERROR", // after register failure
   "REGISTERED", // showing instructional dialog, after register success
-  "NEEDS_VERIFICATION",
+  "NEEDS_VERIFICATION", // showing instructional dialog with resend link
+  "VERIFICATION_ERROR", // error sending verification, showing error dialog
+  "RESETTING_PASSWORD", // showing reset password dialog
 ] as const;
 
 type LoginState = typeof LoginStates[number];
