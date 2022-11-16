9c25800766676289ebc0d0a67690001c9960374f
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-16 05:52:03 -0500

    prop drill data-testid in Link

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -5,11 +5,12 @@ import MuiLink from "@mui/material/Link";
 interface LinkProps {
   children: React.ReactNode;
   onClick?: React.MouseEventHandler<HTMLElement>;
+  "data-testid"?: string;
 }
 
-const Link = ({ children, onClick }: LinkProps) => {
+const Link = ({ children, onClick, "data-testid": dtid }: LinkProps) => {
   return (
-    <MuiLink onClick={onClick} sx={{ "&:hover": { cursor: "pointer" } }}>{children}</MuiLink>
+    <MuiLink onClick={onClick} sx={{ "&:hover": { cursor: "pointer" } }} data-testid={dtid}>{children}</MuiLink>
   );
 };
 
