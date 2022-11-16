a6554166e34554d92f0fcc777d74ebcc5ad11117
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-16 10:41:01 -0500

    stub in tests for VerificationEmailErrorDialog

---
+++ src/components/molecules/__tests__/VerificationEmailErrorDialog.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+describe('VerificationEmailErrorDialog', () => {
+  it.todo('does not render when LoginState=%p');
+  it.todo('renders when LoginState="VERIFICATION_ERROR"');
+  it.todo('sets LoginState to LOGGED_OUT after close');
+});
