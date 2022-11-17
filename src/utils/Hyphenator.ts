8bde30b7bd70a8fdb7e13e26f01febefe907c12f
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-17 13:10:48 -0500

    create Hyphenator function with tests

---
+++ src/utils/Hyphenator.ts
@@ -0,0 +1,30 @@
+export function hyphenate(input: string, currentValues?: string[]): string {
+  // replace groups of non-word characters with a single hyphen
+  const simple = input.trim().toLowerCase().replaceAll(/[\W_]+/g, "-");
+
+  // if there are no current values, then this is unique. done.
+  if (currentValues === undefined || currentValues.length === 0) {
+    return simple;
+  }
+
+  // extract baseName and index, i.e. "prefix-123" => "prefix", "123"
+  const inputMatches = simple.match(/^(.+)-(\d+)?$/);
+  const baseName = inputMatches?.at(1) || simple;
+  const strIndex = inputMatches?.at(2);
+  let index = strIndex !== undefined ? parseInt(strIndex) : 1;
+
+  console.assert(index > 0, "index is invalid: ", index);
+  const matches = currentValues.filter(val => val.startsWith(baseName));
+  if (matches.length === 0) {
+    return simple;
+  }
+
+  const indexes = matches
+      .map(val => val.match(`${baseName}-(\\d+)`)?.at(1))
+      .map(val => val ? parseInt(val) : 0);
+  if (indexes.includes(index)) {
+    index = Math.max(...indexes) + 1;
+  }
+  return `${baseName}-${index}`;
+}
+
---
+++ src/utils/__tests__/Hyphenator.test.ts
@@ -0,0 +1,68 @@
+import { hyphenate } from "../Hyphenator";
+
+describe('hyphenate(*, [])', () => {
+  describe('returns input when only aphanumeric', () => {
+    it.each(
+      [
+        "test123",
+        "hello",
+        "1234567",
+        "123test123",
+      ]
+    )('%p remains unchanged', (input: string) => {
+      expect(hyphenate(input)).toEqual(input);
+    });
+  });
+
+  describe('trims leading and trailing spaces', () => {
+    it.each([
+      [" test ", "test"],
+      ["\ttest", "test"],
+      ["\n\rtest543\t", "test543"],
+    ])('%p returns %p', (input: string, expected: string) => {
+      expect(hyphenate(input)).toEqual(expected);
+    });
+  });
+
+  describe('converts subsequent non-word characters into a hyphen', () => {
+    it.each(
+      [
+        ["hello_you", "hello-you"],
+        ["hello world", "hello-world"],
+        ["hello*&^world", "hello-world"],
+        ["hello---world", "hello-world"],
+        ["123-3.2", "123-3-2"]
+      ]
+    )('converts %p into %p', (input: string, expected: string) => {
+      expect(hyphenate(input)).toEqual(expected);
+    });
+  });
+});
+
+describe('hyphenate("hw", [x, ...])', () => {
+  it.each([ // input will be "hw" for each
+    [["hw"], 1],
+    [["hw-1"], 2],
+    [["hw-2"], 1],
+    [["hw-3", "wh"], 1],
+    [["hw", "hw-2"], 1],
+    [["hw", "hw-2", "hw-1"], 3],
+    [["hw-1", "hw-2","hw-3"], 4],
+    [["hw-1","hw-3"], 4],
+  ])('given "hw" with exising %p, expect "hw-%p"', (currentValues: string[], expectedSuffix: number) => {
+    expect(hyphenate("hw", currentValues)).toEqual(`hw-${expectedSuffix}`);
+  });
+});
+
+describe('hyphenate(*, [*, ...])', () => {
+  it.each([
+    ["hw-1", ["hw-1", "hw-2"], "hw-3"],
+    ["hw-2", ["hw-1", "hw-2", "hw-3"], "hw-4"],
+    ["h-=*=-w-1", ["h-w-1", "h-w-3"], "h-w-4"],
+    ["1.4.3", ["1-4-3"], "1-4-4"], // this is a strange case, but the results just need to be unique; sometimes they won't make sense
+    ["hw-7", ["hw-1", "hw-2"], "hw-7"],
+    ["abc", ["hw", "hw-1", "hw-54"], "abc"],
+  ])('given %p with existing %p, expect %p', (input: string, currentValues: string[], expected: string) => {
+    expect(hyphenate(input, currentValues)).toEqual(expected);
+  });
+});
