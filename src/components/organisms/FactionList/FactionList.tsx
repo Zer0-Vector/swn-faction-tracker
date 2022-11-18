c4efa6738d8b62dc2ad6a1f94ac258e03b4b539f
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-18 08:46:48 -0500

    update FactionList to useFactionSelection

--- src/components/organisms/FactionList/FactionList.tsx
+++ src/components/organisms/FactionList/FactionList.tsx
@@ -8,7 +8,7 @@ import { useTheme } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 
 import { GameContext } from "../../../contexts/GameContext";
-import { UiStateContext } from "../../../contexts/UiStateContext";
+import { useFactionSelection } from "../../../hooks/useFactionSelection";
 import FactionInfo from "../../../types/FactionInfo";
 import AssetList from "../AssetList";
 import AssetListActionsToolbar from "../AssetListActionsToolbar";
@@ -18,8 +18,8 @@ import FactionListItem from "./FactionListItem";
 
 export default function FactionList(): JSX.Element {
   const { state, controller } = useContext(GameContext);
-  const { state: uiState } = useContext(UiStateContext);
   const theme = useTheme();
+  const { faction: selectedFaction } = useFactionSelection();
   
   const handleDragEnd = (result: DropResult) => {
     if (!result.destination) {
@@ -73,7 +73,7 @@ export default function FactionList(): JSX.Element {
                         isDragging={itemSnapshot.isDragging}
                       />
                       <Collapse
-                        in={uiState.selectedFaction === faction.id}
+                        in={selectedFaction?.id === faction.id}
                         unmountOnExit={true}
                       >
                         <Box sx={{
