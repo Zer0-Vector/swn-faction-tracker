4f1b8680305b46b0056a2449618df94cacf1d8e2
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-20 10:02:09 -0500

    implement a few tests for ConfirmDialog

--- src/components/molecules/ConfirmDialog/ConfirmDialog.test.tsx
+++ src/components/molecules/ConfirmDialog/ConfirmDialog.test.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 
+import { render, screen } from "@testing-library/react";
+
+import ConfirmDialog from "./ConfirmDialog";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const NOP = ()=>{};
+
+function renderDumb(open = true, id = "test") {
+  render(<ConfirmDialog id={id} open={open} title="test-title" message="test-message" onCancel={NOP} onConfirm={NOP} />);
+}
+
 describe('default ConfirmDialog', () => {
-  it.todo('does not render when open=false');
-  it.todo('renders when open=true');
-  it.todo('renders given title');
-  it.todo('renders given message');
+  it('does not render when open=false', () => {
+    renderDumb(false);
+    expect(screen.queryByTestId("test-confirm-dialog")).not.toBeInTheDocument();
+  });
+
+  it('renders when open=true', () => {
+    renderDumb();
+    expect(screen.getByTestId("test-confirm-dialog")).toBeInTheDocument();
+  });
+
+  it('renders given title', () => {
+    renderDumb();
+    const title = screen.getByTestId("test-confirm-dialog-title");
+    expect(title).toBeInTheDocument();
+    expect(title.textContent).toEqual("test-title");
+  });
+
+  it('renders given message', () => {
+    renderDumb();
+    const message = screen.getByTestId("test-confirm-dialog-content-text");
+    expect(message).toBeInTheDocument();
+    expect(message.textContent).toEqual("test-message");
+  });
+
   it.todo('renders given button text');
   it.todo('renders buttons');
   it.todo('calls onClose when cancelled');
