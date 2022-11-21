0a99039bb98ccd7f2e5a3da458e56c73a10378cd
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-21 16:50:28 -0500

    validate state setter for addFaction and addAsset

--- src/controllers/GameController.test.ts
+++ src/controllers/GameController.test.ts
@@ -1,4 +1,6 @@
+import PurchasedAsset from "../types/PurchasedAsset";
 import RuntimeGameState from "../types/RuntimeGameState";
+import StoredGameState from "../types/StoredGameState";
 
 import { GameController, GameStateSetter } from "./GameController";
 
@@ -22,18 +24,34 @@ describe('GameController', () => {
 
   it('addFaction', () => {
     const rtgsAddFaction = jest.spyOn(gameState, 'addFaction');
-    const result = gameController.addFaction("Test Faction");
-    expect(result).not.toBeUndefined();
+    const faction = gameController.addFaction("Test Faction");
+    expect(faction).not.toBeUndefined();
     expect(rtgsAddFaction).toBeCalledTimes(1);
     expect(setFn).toBeCalledTimes(1);
+
+    const setter = setFn.mock.calls[0][0] as (prev: StoredGameState) => StoredGameState;
+    const result = setter({} as StoredGameState);
+    expect(result).not.toBeUndefined();
+    expect(result.factions.length).toEqual(1);
+    expect(result.factionOrder.length).toEqual(1);
+    expect(result.factionOrder[0]).toEqual(faction.id);
+    expect(result.factions[0][1]).toEqual(faction);
   });
 
   it('addAsset', () => {
     const rtgsAddAsset = jest.spyOn(gameState, 'addAsset');
-    const result = gameController.addAsset("test-faction", "Asset Test");
-    expect(result).not.toBeUndefined();
+    const asset = gameController.addAsset("test-faction", "Asset Test");
+    expect(asset).not.toBeUndefined();
     expect(rtgsAddAsset).toBeCalledTimes(1);
     expect(rtgsAddAsset).toBeCalledWith("test-faction", "Asset Test");
     expect(setFn).toBeCalledTimes(1);
+    const setter = setFn.mock.calls[0][0] as (prev: StoredGameState) => StoredGameState;
+    const result = setter({} as StoredGameState);
+    expect(result).not.toBeUndefined();
+    expect(result.assets.length).toEqual(1);
+    expect(result.assets[0][1]).toEqual({
+      id: { displayName: "Asset Test", index: 1 },
+      hp: 0,
+    } as PurchasedAsset);
   });
 });
