5f43f1cc6cd27f8958228b11551e34a5d1745e34
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-21 09:23:40 -0500

    use TestableProps: FactionStatSummary,GoalProgress

--- src/components/molecules/FactionStatSummary/FactionStatSummary.tsx
+++ src/components/molecules/FactionStatSummary/FactionStatSummary.tsx
@@ -1,24 +1,25 @@
 import React, { useContext } from "react";
 
 import { GameContext } from "../../../contexts/GameContext";
+import TestableProps from "../../../types/TestableProps";
 import EditableStatText from "../../atoms/EditableStatText";
 import StatText from "../../atoms/StatText";
 
 
-interface FactionStatSummaryProps {
+interface FactionStatSummaryProps extends TestableProps {
   factionId: string;
   force: number;
   cunning: number;
   wealth: number;
 }
 
-export default function FactionStatSummary({ factionId, force, cunning, wealth }: FactionStatSummaryProps) {
+export default function FactionStatSummary({ factionId, force, cunning, wealth, "data-testid": dtid }: FactionStatSummaryProps) {
   const { controller } = useContext(GameContext);
   
   return (
     <>
       <EditableStatText
-        id="stat-force"
+        data-testid={`${dtid}-force`}
         updateValue={val => controller.updateForce(factionId, +val)}
         inputSx={theme => ({ 
           ...theme.typography.body2,
@@ -29,7 +30,7 @@ export default function FactionStatSummary({ factionId, force, cunning, wealth }
       </EditableStatText>
       <StatText variant="body2">/</StatText>
       <EditableStatText
-        id="stat-cunning"
+        data-testid={`${dtid}-cunning`}
         updateValue={val => controller.updateCunning(factionId, +val)}
         inputSx={theme => ({ 
           ...theme.typography.body2,
@@ -40,7 +41,7 @@ export default function FactionStatSummary({ factionId, force, cunning, wealth }
       </EditableStatText>
       <StatText variant="body2">/</StatText>
       <EditableStatText
-        id="stat-wealth"
+        data-testid={`${dtid}-wealth`}
         updateValue={val => controller.updateWealth(factionId, +val)}
         inputSx={theme => ({ 
           ...theme.typography.body2,
