aefa6aa92feaec051a536b2373b928000c7be3bc
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-21 10:44:15 -0500

    remove references to uiController selection method

--- src/components/organisms/LocationsActionToolbar/LocationsActionToolbar.tsx
+++ src/components/organisms/LocationsActionToolbar/LocationsActionToolbar.tsx
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { GameContext } from "../../../contexts/GameContext";
-import { UiStateContext } from "../../../contexts/UiStateContext";
+import { useSelection } from "../../../hooks/useSelection";
 import LocationInfo from "../../../types/LocationInfo";
 import AddLocationDialog from "../../molecules/AddLocationDialog";
 import ConfirmDialog from "../../molecules/ConfirmDialog";
@@ -9,9 +10,10 @@ import ListActionToolbar from "../../molecules/ListActionToolbar";
 
 export default function LocationsActionToolbar() {
   const { controller } = useContext(GameContext);
-  const { state: uiState, controller: uiController } = useContext(UiStateContext);
   const [addDialogOpen, setAddDialogOpen] = useState<boolean>(false);
   const [removeDialogOpen, setRemoveDialogOpen] = useState<boolean>(false);
+  const { location: selectedLocation } = useSelection();
+  const nav = useNavigate();
 
   const handleOpenAddDialog: React.MouseEventHandler<HTMLButtonElement> = (evt) => {
     evt.stopPropagation();
@@ -33,16 +35,16 @@ export default function LocationsActionToolbar() {
   const handleCloseRemoveDialog = () => setRemoveDialogOpen(false);
 
   const handleRemove = () => {
-    if (uiState.selectedLocation) {
-      controller.removeLocation(uiState.selectedLocation);
-      uiController.selectLocaion(null);
+    if (selectedLocation) {
+      controller.removeLocation(selectedLocation.id);
+      nav("/locations");
     }
     handleCloseRemoveDialog();
   };
 
   return (
     <ListActionToolbar
-      removable={!!uiState.selectedLocation}
+      removable={!!selectedLocation}
       onAddClick={handleOpenAddDialog}
       onRemoveClick={handleOpenRemoveDialog}
     >
@@ -57,7 +59,7 @@ export default function LocationsActionToolbar() {
         onCancel={handleCloseRemoveDialog}
         onConfirm={handleRemove}
         title="Confirm Remove Location"
-        message={`Remove location '${uiState.selectedLocation}'?`}
+        message={`Remove location '${selectedLocation?.name}'?`}
         buttonText="Remove"
       />
     </ListActionToolbar>
