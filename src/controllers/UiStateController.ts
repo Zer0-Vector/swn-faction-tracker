cc9c2d2fd6e63c423930a4a9b8f65c484d95f91e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-21 10:44:34 -0500

    remove selection from UiState/UiStateController

--- src/controllers/UiStateController.ts
+++ src/controllers/UiStateController.ts
@@ -1,14 +1,8 @@
 import LoginState from "../types/LoginState";
-import Nullable from "../types/Nullable";
-import PurchasedAsset, { PurchasedAssetUtils } from "../types/PurchasedAsset";
 import UiState from "../types/UiState";
 
 export interface IUiStateController {
   setLoginState(state: LoginState): void;
-  selectLocaion(locationName: Nullable<string>): void;
-  selectFaction(name: Nullable<string>): void;
-  selectAsset(pa: Nullable<PurchasedAsset>) : void;
-  clearSelections(): void;
 }
 
 type UiStateSetter = React.Dispatch<React.SetStateAction<UiState>>;
@@ -25,45 +19,6 @@ export class UiStateController implements IUiStateController {
     this.setState(prev => ({
       ...prev,
       loginState: state,
-    }));
-  }
-
-  selectFaction(name: Nullable<string>): void {
-    this.setState((prev: UiState) => {
-      if (prev.selectedFaction === name) {
-        return prev;
-      }
-
-      return {
-        ...prev,
-        hasFactionSelected: name !== null,
-        selectedAssetKey: null,
-        selectedFaction: name,
-      };
-    });
-  }
-
-  selectAsset(pa: Nullable<PurchasedAsset>): void {
-    this.setState(prev => ({
-      ...prev,
-      selectedAssetKey: pa === null ? null : PurchasedAssetUtils.getKey(prev.selectedFaction as string, pa as PurchasedAsset)
-    }));
-  }
-
-  selectLocaion(locationName: Nullable<string>): void {
-    this.setState(prev => ({
-      ...prev,
-      selectedLocation: locationName,
-    }));
-  }
-
-  clearSelections(): void {
-    this.setState(prev => ({
-      ...prev,
-      selectedFaction: null,
-      selectedAssetKey: null,
-      selectedLocation: null,
-      hasFactionSelected: false,
     }));
   }
 
