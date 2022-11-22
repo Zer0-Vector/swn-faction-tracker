b02f12cca1e32dfd61d038d0e6e787fdec6705bf
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-22 13:38:39 -0500

    rework FactionHpSummary and test

--- src/components/molecules/FactionHpSummary/FactionHpSummary.tsx
+++ src/components/molecules/FactionHpSummary/FactionHpSummary.tsx
@@ -4,15 +4,14 @@ import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
 
 import { GameContext } from "../../../contexts/GameContext";
-import TestableProps from "../../../types/TestableProps";
 import EditableStatText from "../../atoms/EditableStatText";
 import StatText from "../../atoms/StatText";
 
-interface FactionHpSummaryProps extends TestableProps {
+interface FactionHpSummaryProps {
   factionId: string;
 }
 
-export default function FactionHpSummary({ factionId, "data-testid": dtid }: FactionHpSummaryProps) {
+export default function FactionHpSummary({ factionId }: FactionHpSummaryProps) {
   const { state, controller } = useContext(GameContext);
 
   const HpBox = styled(Box)(theme => ({
@@ -24,8 +23,8 @@ export default function FactionHpSummary({ factionId, "data-testid": dtid }: Fac
   console.debug(`faction(${factionId}) =`, faction);
   if (!faction) {
     return (
-      <HpBox>
-        <StatText color="warning.main" data-testid={dtid}>??/??</StatText>
+      <HpBox data-testid="faction-hp-box">
+        <StatText color="warning.main">??/??</StatText>
       </HpBox>
     );
   }
@@ -40,16 +39,16 @@ export default function FactionHpSummary({ factionId, "data-testid": dtid }: Fac
   };
 
   return (
-    <HpBox>
+    <HpBox data-testid="faction-hp-box">
       <EditableStatText
         updateValue={handleUpdate}
         inputSx={{ maxWidth: "4em"}}
-        data-testid={`${dtid}-hp`}
+        data-testid="hp"
       >
         {hp}
       </EditableStatText>
       <StatText>/</StatText>
-      <StatText data-testid={`${dtid}-maxhp`}>{maxHp}</StatText>
+      <StatText data-testid="maxhp">{maxHp}</StatText>
     </HpBox>
   );
 }
