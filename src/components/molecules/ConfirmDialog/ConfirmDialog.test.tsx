682def6e1235b8730fbe2dc390af87e615538778
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-22 09:09:29 -0500

    update ConfirmDialog data-testid values

--- src/components/molecules/ConfirmDialog/ConfirmDialog.test.tsx
+++ src/components/molecules/ConfirmDialog/ConfirmDialog.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, within } from "@testing-library/react";
 
 import { Maybe } from "../../../types/Maybe";
 
@@ -10,54 +10,57 @@ import ConfirmDialog from "./ConfirmDialog";
 const mockCancel = jest.fn();
 const mockConfirm = jest.fn();
 
-function renderDumb(open = true, buttonText: Maybe<string> = undefined, id = "test") {
+function renderIt(open = true, buttonText: Maybe<string> = undefined) {
   render(
     <ConfirmDialog
-      id={id}
       open={open}
       title="test-title"
       message="test-message"
       onCancel={mockCancel}
       onConfirm={mockConfirm}
       buttonText={buttonText}
+      data-testid="test-dialog"
     />
   );
 }
 
 describe('default ConfirmDialog', () => {
   it('does not render when open=false', () => {
-    renderDumb(false);
-    expect(screen.queryByTestId("test-confirm-dialog")).not.toBeInTheDocument();
+    renderIt(false);
+    expect(screen.queryByTestId("test-dialog")).not.toBeInTheDocument();
   });
 
   it('renders when open=true', () => {
-    renderDumb();
-    expect(screen.getByTestId("test-confirm-dialog")).toBeInTheDocument();
+    renderIt();
+    expect(screen.getByTestId("test-dialog")).toBeInTheDocument();
   });
 
   it('renders given title', () => {
-    renderDumb();
-    const title = screen.getByTestId("test-confirm-dialog-title");
+    renderIt();
+    const dialog = screen.getByTestId("test-dialog");
+    const title = within(dialog).getByTestId("title");
     expect(title).toBeInTheDocument();
     expect(title.textContent).toEqual("test-title");
   });
 
   it('renders given message', () => {
-    renderDumb();
-    const message = screen.getByTestId("test-confirm-dialog-content-text");
+    renderIt();
+    const dialog = screen.getByTestId("test-dialog");
+    const message = within(dialog).getByTestId("content-text");
     expect(message).toBeInTheDocument();
     expect(message.textContent).toEqual("test-message");
   });
 
   it('renders buttons', () => {
-    renderDumb();
-    const cancel = screen.getByTestId("test-confirm-dialog-cancel-button");
+    renderIt();
+    const dialog = screen.getByTestId("test-dialog");
+    const cancel = within(dialog).getByTestId("cancel-button");
     expect(cancel).toBeInTheDocument();
     expect(cancel).toBeInstanceOf(HTMLButtonElement);
     expect(cancel).not.toBeDisabled();
     expect(cancel.textContent).toEqual("Cancel");
 
-    const confirm = screen.getByTestId("test-confirm-dialog-confirm-button");
+    const confirm = within(dialog).getByTestId("confirm-button");
     expect(confirm).toBeInTheDocument();
     expect(confirm).toBeInstanceOf(HTMLButtonElement);
     expect(confirm).not.toBeDisabled();
@@ -65,8 +68,9 @@ describe('default ConfirmDialog', () => {
   });
 
   it('renders given button text', () => {
-    renderDumb(true, "OK");
-    const confirm = screen.getByTestId("test-confirm-dialog-confirm-button");
+    renderIt(true, "OK");
+    const dialog = screen.getByTestId("test-dialog");
+    const confirm = within(dialog).getByTestId("confirm-button");
     expect(confirm).toBeInTheDocument();
     expect(confirm).toBeInstanceOf(HTMLButtonElement);
     expect(confirm).not.toBeDisabled();
@@ -74,16 +78,18 @@ describe('default ConfirmDialog', () => {
   });
 
   it('calls onCancel when cancelled', () => {
-    renderDumb();
-    const cancel = screen.getByTestId("test-confirm-dialog-cancel-button");
+    renderIt();
+    const dialog = screen.getByTestId("test-dialog");
+    const cancel = within(dialog).getByTestId("cancel-button");
     fireEvent.click(cancel);
     expect(mockCancel).toBeCalledTimes(1);
     expect(mockConfirm).not.toBeCalled();
   });
   
   it('calls onConfirm when confirmed', () => {
-    renderDumb();
-    const confirm = screen.getByTestId("test-confirm-dialog-confirm-button");
+    renderIt();
+    const dialog = screen.getByTestId("test-dialog");
+    const confirm = within(dialog).getByTestId("confirm-button");
     fireEvent.click(confirm);
     expect(mockConfirm).toBeCalledTimes(1);
     expect(mockCancel).not.toBeCalled();
