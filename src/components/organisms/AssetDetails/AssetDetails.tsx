e5d67bc2d7d149b6cf68cfe24d1f1d607c4f1625
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-22 06:28:42 -0500

    memoize AssetDetails

--- src/components/organisms/AssetDetails/AssetDetails.tsx
+++ src/components/organisms/AssetDetails/AssetDetails.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 
 import Card from "@mui/material/Card";
 import Grid, { GridSize } from "@mui/material/Grid";
@@ -31,16 +31,18 @@ interface LabeledItemProps {
   xsContent?: GridSize;
 }
 
-function LabeledItem({ label, children, sx, xsLabel, xsContent }: LabeledItemProps) {
+const LabeledItemComponent = ({ label, children, sx, xsLabel, xsContent }: LabeledItemProps) => {
+  const gridSx = useMemo(() => ({
+    fontWeight: "bold",
+    textAlign: "right",
+    overflow: "clip",
+    textOverflow: "ellipsis",
+  }), []);
+
   return (
     <Item sx={sx}>
       <Grid container columnSpacing={2}>
-        <Grid item xs={xsLabel || "auto"} sx={{
-          fontWeight: "bold",
-          textAlign: "right",
-          overflow: "clip",
-          textOverflow: "ellipsis",
-        }}>
+        <Grid item xs={xsLabel || "auto"} sx={gridSx}>
           {label}
         </Grid>
         <Grid item xs={xsContent || "auto"} sx={{
@@ -51,34 +53,54 @@ function LabeledItem({ label, children, sx, xsLabel, xsContent }: LabeledItemPro
       </Grid>
     </Item>
   );
-}
+};
 
-export default function AssetDetails({ asset }: AssetDetailsProps) {
+const LabeledItem = React.memo(LabeledItemComponent);
+
+const AssetDetailsComponent = ({ asset }: AssetDetailsProps) => {
+  const cardSx = useMemo<SxProps>(() => ({
+    backgroundColor: "background.paper2",
+    display: "grid",
+    gridTemplateColumns: "3fr 2fr",
+    gap: 1.5,
+    padding: 2,
+  }), []);
+  
+  const descriptionSx = useMemo(() => ({
+    textAlign: "justify",
+    gridRow: "1 / 5",
+    alignItems: "flex-start"
+  }), []);
+
+  const attributeSx = useMemo(() => ({
+    textTransform: "uppercase",
+    fontStyle: "italic"
+  }), []);
+  
+  const dmgText = useCallback((result: AssetAttackResult) => (
+    result.type === "DAMAGE" 
+    ? result.damage 
+    : "Special"
+  ), []);
+  
   const assetInfo = ASSETS[asset.id.displayName];
   if (!assetInfo) {
     console.error(`Could not find asset named '${asset.id.displayName}'`);
-    return (<Typography color="error.main" fontWeight="bold" fontStyle="italic">Could not load asset info</Typography>);
+    return (<Typography color="error" fontWeight="bold" fontStyle="italic">Could not load asset info</Typography>);
   }
 
   const { hp } = asset;
   const { description, attack, counter, attribute, level, maxHp, type, upkeep, /*note*/ } = assetInfo;
   const attributeText = `${attribute} ${level}`;
-  const dmgText = (result: AssetAttackResult) => result.type === "DAMAGE" ? result.damage : "Special";
   const attackText = attack ? `${TextUtils.titleCase(attack.offense)} vs. ${TextUtils.titleCase(attack.defense)}, ${dmgText(attack.result)}` : "None";
   const counterText = counter ? counter : "None";
   const typeText = TextUtils.titleCase(type.replaceAll(/_/g, " "));
   const upkeepText = upkeep === 0 ? "None" : `${upkeep} FacCred`;
 
   return (
-    <Card sx={{
-      backgroundColor: "background.paper2",
-      display: "grid",
-      gridTemplateColumns: "3fr 2fr",
-      gap: 1.5,
-      padding: 2,
-    }}>
-      <Item sx={{ textAlign: "justify", gridRow: "1 / 5", alignItems: "flex-start" }}>{description}</Item>
-      <Item sx={{ textTransform: "uppercase", fontStyle: "italic" }}>{attributeText}</Item>
+    <Card sx={cardSx}>
+      <Item sx={descriptionSx}>{description}</Item>
+      <Item sx={attributeSx}>{attributeText}</Item>
       {/* TODO use HealthDisplay or refactor it */}
       <Item>{hp}/{maxHp}</Item>
       <Item>{typeText}</Item>
@@ -87,4 +109,8 @@ export default function AssetDetails({ asset }: AssetDetailsProps) {
       <LabeledItem label="Counter" xsLabel={6}>{counterText}</LabeledItem>
     </Card>
   );
-}
+};
+
+const AssetDetails = React.memo(AssetDetailsComponent);
+
+export default AssetDetails;
