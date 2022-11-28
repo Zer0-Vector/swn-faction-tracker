a053844f26e1cb945587bad8dd2677bc192cd9ad
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-27 21:31:18 -0500

    stub in tests for EditableDropDownText

---
+++ src/components/atoms/EditableDropDownText/EditableDropDownText.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+describe('default EditableDropDownText', () => {
+  it.todo('renders text on init');
+  it.todo('redners dropdown after edit button clicked');
+  it.todo('calls onUpdate if selection changes');
+  it.todo('cancels update on Escape keyUp');
+});
