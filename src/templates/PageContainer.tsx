978d059856221df496a17b3b3d569a938663a57e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-29 09:41:58 -0500

    memoize style props for PageContainer

--- src/templates/PageContainer.tsx
+++ src/templates/PageContainer.tsx
@@ -6,7 +6,7 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
+import Grid, { GridSize } from "@mui/material/Grid";
 import { SxProps, Theme } from "@mui/material/styles";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
@@ -21,6 +21,17 @@ export interface PageContainerProps {
   children?: React.ReactNode;
 }
 
+const GridItem = ({ xs, children }: { xs: GridSize, children: React.ReactNode }) => (
+  <Grid item
+    xs={xs}
+    display="flex"
+    justifyContent="flex-start"
+    alignItems="center"
+  >
+    {children}
+  </Grid>
+);
+
 export default function PageContainer({ children }: PageContainerProps) {
   const location = useLocation();
 
@@ -28,32 +39,50 @@ export default function PageContainer({ children }: PageContainerProps) {
 
   console.debug("Rendering PageContainer...");
 
-  const tabSx: SxProps<Theme> = useMemo(() => ({
+  const tabSx = useMemo<SxProps<Theme>>(() => ({
     color: "primary.contrastText",
     '&:hover': {
       color: "primary.light",
+      backgroundColor: "action.hover",
     }
+  }), []);
+
+  const boxSx = useMemo<SxProps<Theme>>(() => ({
+    display: "flex",
+    minHeight: "100vh",
+    flexDirection: "column",
+    alignItems: "stretch",
+  }), []);
+
+  const appBarSx = useMemo<SxProps<Theme>>(() => ({ 
+    color: "primary.contrastText",
+    backgroundColor: "primary.dark",
+  }), []);
+
+  const appTitleSx = useMemo<SxProps<Theme>>(() => ({
+    mr: 2,
+    color: "primary.contrastText",
+    whiteSpace: "nowrap"
+  }), []);
+
+  const containerSx = useMemo<SxProps<Theme>>(() => ({
+    py: 2,
+    width: "100vw",
+    display: "grid",
+    gridTemplateColumns: "40% 60%",
   }), []);
 
   return (
     <>
       <CssBaseline />
-      <Box sx={{
-          display: "flex",
-          minHeight: "100vh",
-          flexDirection: "column",
-          alignItems: "stretch",
-        }}
-        data-testid="page-container"
-      >
-        
-        <AppBar sx={{ color: "primary.contrastText", backgroundColor: "primary.dark" }}>
+      <Box sx={boxSx} data-testid="page-container">
+        <AppBar sx={appBarSx}>
           <Toolbar>
-            <Grid container>
-              <Grid item xs={3.5}>
-                <Typography variant="h1" sx={{ mr: 2, color: "primary.contrastText", whiteSpace: "nowrap" }}>SWN Faction Tracker</Typography>
-              </Grid>
-              <Grid item xs={4.5}>
+            <Grid container>{/* TODO use different layout container */}
+              <GridItem xs={3.5}>
+                <Typography variant="h1" sx={appTitleSx}>SWN Faction Tracker</Typography>
+              </GridItem>
+              <GridItem xs={4.5}>
                 <Tabs value={tab} component="nav">
                   <Tab
                     value="FACTIONS"
@@ -70,23 +99,18 @@ export default function PageContainer({ children }: PageContainerProps) {
                     sx={tabSx}
                     />
                 </Tabs>
-              </Grid>
-              <Grid item xs={3}>
+              </GridItem>
+              <GridItem xs={3}>
                 <ModeToggleButtons />
-              </Grid>
-              <Grid item xs={1}>
+              </GridItem>
+              <GridItem xs={1}>
                 <UserMenu user={getAuth(FirebaseApp).currentUser} />
-              </Grid>
+              </GridItem>
             </Grid>
           </Toolbar>
         </AppBar>
         <Toolbar id="appbar-shim" />
-        <Container sx={theme => ({
-          py: theme.spacing(2),
-          width: "100vw",
-          display: "grid",
-          gridTemplateColumns: "40% 60%",
-        })}>
+        <Container sx={containerSx}>
           {children}
         </Container>
       </Box>
