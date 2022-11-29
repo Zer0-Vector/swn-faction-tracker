b943a7cb9059f004373c78fcbef4885711cafecb
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-29 09:50:08 -0500

    memoize box style for *Panel

--- src/pages/LocationsPanel.tsx
+++ src/pages/LocationsPanel.tsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import Box from "@mui/material/Box";
+import { SxProps, Theme } from "@mui/material/styles";
 
 import LocationsActionToolbar from "../components/organisms/LocationsActionToolbar";
 import LocationsList from "../components/organisms/LocationsList";
 
 export default function LocationsPanel() {
+  const boxSx = useMemo<SxProps<Theme>>(() => ({
+    gridColumn: "1 / 3",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    width: "100%"
+  }), []);
+  
+  console.debug("Rendering LocationsPanel...");
   return (
-    <Box sx={theme => ({
-      gridColumn: "1 / 3",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      width: "100%"
-    })}>
+    <Box sx={boxSx}>
       <LocationsActionToolbar />
       <LocationsList />
     </Box>
--- src/pages/PrimaryPanel.tsx
+++ src/pages/PrimaryPanel.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import Box from "@mui/material/Box";
+import { SxProps, Theme } from "@mui/material/styles";
 
 import FactionList from "../components/organisms/FactionList";
 import FactionListActionToolbar from "../components/organisms/FactionListActionToolbar";
 
 export default function PrimaryPanel() {
+  console.debug("Rendering PrimaryPanel...");
+  const boxSx = useMemo<SxProps<Theme>>(() => ({ gridColumnStart: "1", gridColumnEnd: "3" }), []);
   return (
-    <Box sx={{ gridColumnStart: "1", gridColumnEnd: "3" }}>
+    <Box sx={boxSx}>
       <FactionListActionToolbar />
       <FactionList />
     </Box>
