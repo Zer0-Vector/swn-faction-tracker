963219065ef7582700f6103dcb05b213fd11a3f0
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-29 06:39:40 -0500

    create ValidatedTextField.stories

---
+++ src/components/atoms/ValidatedTextField/ValidatedTextField.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { ValidationContext } from "../../../contexts/ValidationContext";
+import { ValidationController } from "../../../controllers/ValidationController";
+
+import { ValidatedTextField } from "./ValidatedTextField";
+
+export default {
+  component: ValidatedTextField,
+} as ComponentMeta<typeof ValidatedTextField>;
+
+const Template: ComponentStory<typeof ValidatedTextField> = args => {
+  return (
+    <ValidationContext.Provider value={new ValidationController({
+      ["test-123"]: (val) => val.length > 0 && val !== "fail",
+    })}>
+      <ValidatedTextField {...args} />
+    </ValidationContext.Provider>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  id: "test-123",
+};
