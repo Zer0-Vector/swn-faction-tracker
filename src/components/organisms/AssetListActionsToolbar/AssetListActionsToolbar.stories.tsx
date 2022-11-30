2d895f22ff6e1db723e077edef2c473f91af9e5f
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-30 11:23:03 -0500

    add AssetListActionsToolbar.stories

---
+++ src/components/organisms/AssetListActionsToolbar/AssetListActionsToolbar.stories.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { GameContext } from "../../../contexts/GameContext";
+import { IGameController } from "../../../controllers/GameController";
+import FactionInfo from "../../../types/FactionInfo";
+import { IGameState } from "../../../types/RuntimeGameState";
+import { MockAction } from "../../__mocks__/MockAction";
+
+import AssetListActionsToolbar from "./AssetListActionsToolbar";
+
+export default {
+  component: AssetListActionsToolbar,
+  decorators: [
+    story => (
+      <GameContext.Provider value={{
+        state: {
+          getFaction(factionId) {
+            return {} as FactionInfo;
+          },
+          getAsset(factionId, assetId) {
+            return {
+              hp: 1,
+              id: {
+                displayName: "Smugglers",
+                index: 1,
+              },
+            };
+          },
+        } as IGameState,
+        controller: {
+          ...MockAction("addAsset"),
+          ...MockAction("removeAsset"),
+        } as unknown as IGameController,
+      }}>
+        {story()}
+      </GameContext.Provider>
+    ),
+  ],
+} as ComponentMeta<typeof AssetListActionsToolbar>;
+
+const Template: ComponentStory<typeof AssetListActionsToolbar> = () => <AssetListActionsToolbar />;
+
+export const Default = Template.bind({});
+Default.decorators = [
+  story => (
+    <MemoryRouter>
+      {story()}
+    </MemoryRouter>
+  ),
+];
+
+export const Removable = Template.bind({});
+Removable.decorators = [
+  story => (
+    <MemoryRouter
+      initialEntries={["/factions/test-faction/assets/smugglers-1"]}
+      initialIndex={0}
+    >
+      {story()}
+    </MemoryRouter>
+  ),
+];
