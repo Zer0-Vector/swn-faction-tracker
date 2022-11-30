1922a5772a6dadb3670b43d43925fc7ebc26efe9
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-30 08:25:59 -0500

    add GoalText.stories

---
+++ src/components/molecules/GoalText/GoalText.stories.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { GameContext } from "../../../contexts/GameContext";
+import { IGameController } from "../../../controllers/GameController";
+import FactionInfo from "../../../types/FactionInfo";
+import { IGameState } from "../../../types/RuntimeGameState";
+import { MockAction } from "../../__tests__/MockAction";
+
+import GoalText from "./GoalText";
+
+export default {
+  component: GoalText,
+  decorators: [
+    story => (
+      <GameContext.Provider value={{
+        state: {} as IGameState,
+        controller: {
+          ...MockAction("setGoal"),
+        } as unknown as IGameController,
+      }}>
+        {story()}
+      </GameContext.Provider>
+    ),
+  ],
+} as ComponentMeta<typeof GoalText>;
+
+const Template: ComponentStory<typeof GoalText> = args => <GoalText {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  faction: {
+    id: "test-faction",
+    goal: {
+      type: "Invincible Valor",
+    },
+  } as FactionInfo,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  faction: {
+    id: "test-faction-2",
+  } as FactionInfo,
+};
