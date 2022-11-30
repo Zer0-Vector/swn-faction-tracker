eb3ed994a27dbad38dc10f71f82e057c9e287d4f
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-30 12:41:41 -0500

    add FactionDetails.stories

---
+++ src/components/organisms/FactionDetails/FactionDetails.stories.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { GameContext } from "../../../contexts/GameContext";
+import { IGameController } from "../../../controllers/GameController";
+import { TagsList } from "../../../data/Tags";
+import { IGameState } from "../../../types/RuntimeGameState";
+import { MockAction } from "../../__mocks__/MockAction";
+
+import FactionDetails from "./FactionDetails";
+
+const MockProvider = ({ children }: { children: React.ReactNode }) => (
+  <GameContext.Provider value={{
+    controller: {
+      ...MockAction("updateHomeworld"),
+      ...MockAction("setGoal"),
+      ...MockAction("updateTag"),
+      ...MockAction("updateForce"),
+      ...MockAction("updateCunning"),
+      ...MockAction("updateWealth"),
+      ...MockAction("updateHp"),
+    } as unknown as IGameController,
+    state: {
+      getLocations() {
+        return [
+          {
+            id: "location-1",
+            name: "Location 1",
+            tl: 1, x: 2, y: 3,
+          },
+          {
+            id: "location-2",
+            name: "Location 2",
+            tl: 4, x: 5, y: 6,
+          },
+        ];
+      },
+    } as IGameState,
+  }}>
+    {children}
+  </GameContext.Provider>
+);
+
+export default {
+  component: FactionDetails,
+  decorators: [
+    story => (
+      <MockProvider>
+        {story()}
+      </MockProvider>
+    ),
+  ],
+} as ComponentMeta<typeof FactionDetails>;
+
+const Template: ComponentStory<typeof FactionDetails> = args => <FactionDetails {...args} />;
+
+export const NoGoal = Template.bind({});
+NoGoal.args = {
+  faction: {
+    id: "test-faction",
+    name: "Test Faction",
+    stats: {
+      cunning: 1,
+      force: 2,
+      hp: 3,
+      maxHp: 4,
+      wealth: 5,
+      xp: 6,
+    },
+  },
+};
+
+export const WithGoal = Template.bind({});
+WithGoal.args = {
+  faction: {
+    id: "test-faction",
+    name: "Test Faction",
+    stats: {
+      cunning: 1,
+      force: 2,
+      hp: 3,
+      maxHp: 4,
+      wealth: 5,
+      xp: 6,
+    },
+    goal: {
+      type: "Wealth of Worlds",
+    },
+  },
+};
+
+export const Full = Template.bind({});
+Full.args = {
+  faction: {
+    id: "test-faction",
+    name: "Test Faction",
+    stats: {
+      cunning: 1,
+      force: 2,
+      hp: 3,
+      maxHp: 4,
+      wealth: 5,
+      xp: 6,
+    },
+    goal: {
+      type: "Wealth of Worlds",
+      tally: 2,
+      target: 3,
+      reward: 11,
+      unit: "FacCred",
+    },
+    tag: TagsList[Math.floor(Math.random() * TagsList.length)],
+    homeworld: "Location 1",
+  },
+};
