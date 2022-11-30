11c860afcc9a56808de2415d3cde21646800faf8
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-30 18:16:37 -0500

    simplified (maybe) AddLocationDialog.stories

--- src/components/molecules/AddLocationDialog/AddLocationDialog.stories.tsx
+++ src/components/molecules/AddLocationDialog/AddLocationDialog.stories.tsx
@@ -1,52 +1,51 @@
-import React from "react";
+import React, { ComponentProps } from "react";
 
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Meta, Story } from "@storybook/react";
 
 import { GameContext } from "../../../contexts/GameContext";
 import { IGameController } from "../../../controllers/GameController";
+import LocationInfo from "../../../types/LocationInfo";
 import { IGameState } from "../../../types/RuntimeGameState";
 
 import AddLocationDialog from "./AddLocationDialog";
 
+type PropsType = ComponentProps<typeof AddLocationDialog>;
+
 export default {
   component: AddLocationDialog,
-} as ComponentMeta<typeof AddLocationDialog>;
-
-const MockedState = {
-  getLocations: () => ([
-    {
-      id: "test-one",
-      name: "Test One",
-    },
-    {
-      id: "test-two",
-      name: "Test Two",
-    },
-  ]),
-} as IGameState;
+} as Meta<PropsType>;
 
 interface MockProviderProps {
   children: React.ReactNode;
-  state: IGameState;
+  locations: LocationInfo[];
 }
 
-const MockProvider = ({ state, children }: MockProviderProps) => (
+const MockProvider = ({ locations, children }: MockProviderProps) => (
   <GameContext.Provider value={{
-    state,
+    state: {
+      getLocations() {
+        return locations;
+      },
+    } as IGameState,
     controller: {} as IGameController,
   }}>
     {children}
   </GameContext.Provider>
 );
 
-export const Default: ComponentStory<typeof AddLocationDialog> = args => <AddLocationDialog {...args} />;
+export const Default: Story<PropsType> = args => (
+  <MockProvider locations={[
+    {
+      id: "existing-location",
+      name: "Existing Location",
+      tl: 0,
+      x: 0,
+      y: 0,
+    },
+  ]}>
+    <AddLocationDialog {...args} />
+  </MockProvider>
+);
 Default.args = {
   open: true,
 };
-Default.decorators = [
-  story => (
-    <MockProvider state={MockedState}>
-      {story()}
-    </MockProvider>
-  ),
-];
