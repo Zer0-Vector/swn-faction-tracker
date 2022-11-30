b1cb7f8c38a8f969fee253d803e3ee7a40fef143
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-11-30 11:08:17 -0500

    add AssetList.stories

---
+++ src/components/organisms/AssetList/AssetList.stories.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { GameContext } from "../../../contexts/GameContext";
+import { IGameController } from "../../../controllers/GameController";
+import PurchasedAsset from "../../../types/PurchasedAsset";
+import { IGameState } from "../../../types/RuntimeGameState";
+
+import AssetList from "./AssetList";
+
+const MockProvider = ({ children, assetList }: { children: React.ReactNode, assetList: PurchasedAsset[] }) => (
+  <GameContext.Provider value={{
+    controller: {} as IGameController,
+    state: {
+      getAssets(factionId) {
+        return assetList;
+      },
+    } as IGameState,
+  }}>
+    {children}
+  </GameContext.Provider>
+);
+
+export default {
+  component: AssetList,
+  decorators: [
+    story => (
+      <MemoryRouter>
+        {story()}
+      </MemoryRouter>
+    ),
+  ],
+} as ComponentMeta<typeof AssetList>;
+
+const Template: ComponentStory<typeof AssetList> = () => <AssetList />;
+
+export const Empty = Template.bind({});
+Empty.decorators = [
+  story => (
+    <MockProvider assetList={[]}>
+      {story()}
+    </MockProvider>
+  ),
+];
+
+export const OneAsset = Template.bind({});
+OneAsset.decorators = [
+  story => (
+    <MockProvider assetList={[
+      {
+        id: {
+          displayName: "Smugglers",
+          index: 1,
+        },
+        hp: 1,
+      },
+    ]}>
+      {story()}
+    </MockProvider>
+  ),
+];
+
+export const ThreeAssets = Template.bind({});
+ThreeAssets.decorators = [
+  story => (
+    <MockProvider assetList={[
+      {
+        id: {
+          displayName: "Smugglers",
+          index: 1,
+        },
+        hp: 1,
+      },
+      {
+        id: {
+          displayName: "Informers",
+          index: 1,
+        },
+        hp: 2,
+      },
+      {
+        id: {
+          displayName: "Blackmail",
+          index: 1,
+        },
+        hp: 0,
+      },
+    ]}>
+      {story()}
+    </MockProvider>
+  ),
+];
