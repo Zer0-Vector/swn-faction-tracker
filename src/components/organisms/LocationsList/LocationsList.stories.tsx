c5b093d667124afe635b9d137651e28f612eeaa6
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-01 08:26:01 -0500

    add LocationsList.stories

---
+++ src/components/organisms/LocationsList/LocationsList.stories.tsx
@@ -0,0 +1,95 @@
+import React, { PropsWithChildren } from "react";
+import { MemoryRouter } from "react-router-dom";
+
+import { Meta, Story } from "@storybook/react";
+
+import { GameContext } from "../../../contexts/GameContext";
+import { RequiredChildrenProps } from "../../../types/ChildrenProps";
+import LocationInfo from "../../../types/LocationInfo";
+import { IGameState } from "../../../types/RuntimeGameState";
+import { ExtendedStoryProps } from "../../__mocks__/ExtendedStoryProps";
+import { MockActionController } from "../../__mocks__/MockActionController";
+import { RandomInt } from "../../__mocks__/RandomNumber";
+
+import LocationsList from "./LocationsList";
+
+const MockLocations: (n: number)=>LocationInfo[] = numLocations => 
+  [...Array(numLocations).keys()]
+  .map(n => ({
+    id: `test-location-${n}`,
+    name: `Test Location ${n}`,
+    tl: RandomInt(5),
+    x: RandomInt(10),
+    y: RandomInt(8),
+  }));
+
+// eslint-disable-next-line react/display-name
+const getContextProvider = (numberOfLocations: number) => ({ children }: RequiredChildrenProps) => {
+  const locations = MockLocations(numberOfLocations);
+  return (
+    <GameContext.Provider value={{
+      controller: MockActionController,
+      state: {
+        getLocations: () => locations,
+        getLocation: (locationId) => locations.find(loc => loc.id === locationId),
+      } as IGameState,
+    }}>
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+interface AdditionalArgs {
+  numberOfLocations: number;
+}
+
+type StoryProps = ExtendedStoryProps<typeof LocationsList, AdditionalArgs>;
+
+export default {
+  component: LocationsList,
+  argTypes: {
+    numberOfLocations: {
+      name: "Number of Locations",
+      type: {
+        name: "number",
+        required: true,
+      },
+    },
+  },
+} as Meta<StoryProps>;
+
+const Template: Story<StoryProps> = args => {
+  const Provider = getContextProvider(args.numberOfLocations);
+  return (
+    <Provider>
+      <LocationsList />
+    </Provider>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  numberOfLocations: 5,
+};
+Default.decorators = [
+  story => (
+    <MemoryRouter>
+      {story()}
+    </MemoryRouter>
+  ),
+];
+
+export const Selected = Template.bind({});
+Selected.args = {
+  numberOfLocations: 4,
+};
+Selected.decorators = [
+  story => (
+    <MemoryRouter
+      initialEntries={["/locations/test-location-1", "/locations"]}
+      initialIndex={0}
+    >
+      {story()}
+    </MemoryRouter>
+  ),
+];
