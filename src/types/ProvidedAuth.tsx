59e526ffbc5a49a9d40433a5315489120cf8d89c
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-04 14:43:11 -0500

    use arrow function values instead of method declarations

--- src/types/ProvidedAuth.tsx
+++ src/types/ProvidedAuth.tsx
@@ -15,12 +15,12 @@ export interface ProvidedAuth {
    * @returns The logged in User
    * @throws FirebaseError if error logging in, including credential mismatch
    */
-  login(email: string, password: string): Promise<User>;
+  login: (email: string, password: string) => Promise<User>;
 
   /**
    * Logs out current User.
    */
-  logout(): Promise<void>;
+  logout: () => Promise<void>;
 
   /**
    * User Sign Up
@@ -29,23 +29,23 @@ export interface ProvidedAuth {
    * @returns The logged in User
    * @throws FirebaseError
    */
-  signup(email: string, password: string): Promise<User>;
+  signup: (email: string, password: string) => Promise<User>;
 
   /**
    * Send verification email for current logged in user.
    */
-  sendEmailVerification(): Promise<void>;
+  sendEmailVerification: () => Promise<void>;
 
   /**
    * Send password reset instructions to the given email.
    * @param email The user's email
    */
-  sendPasswordResetEmail(email: string): Promise<void>;
+  sendPasswordResetEmail: (email: string) => Promise<void>;
 
   /**
    * Confirm password reset with given code and new password.
    * @param code The confirmation code given to the User.
    * @param password The User's new password.
    */
-  confirmPasswordReset(code: string, password: string): Promise<void>;
+  confirmPasswordReset: (code: string, password: string) => Promise<void>;
 }
