27569c2481716f04cfafc7f730de1284e186a44d
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-05 07:56:10 -0500

    add LoginDialog.stories

---
+++ src/components/molecules/LoginDialog/LoginDialog.stories.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { action } from "@storybook/addon-actions";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { AuthContext } from "../../../contexts/AuthContext";
+import { UiStateContext } from "../../../contexts/UiStateContext";
+import { ProvidedAuth } from "../../../types/ProvidedAuth";
+import { MockActionUiStateController } from "../../__mocks__/MockActionUiStateController";
+
+import LoginDialog from "./LoginDialog";
+
+export default {
+  component: LoginDialog,
+  decorators: [
+    story => (
+      <UiStateContext.Provider value={{
+        state: {
+          loginState: "LOGGING_IN",
+        },
+        controller: MockActionUiStateController,
+      }}>
+        {story()}
+      </UiStateContext.Provider>
+    ),
+    story => (
+      <AuthContext.Provider value={{
+        login: (...args) => {
+          action("login")(...args);
+          return Promise.resolve({});
+        },
+      } as ProvidedAuth}>
+        {story()}
+      </AuthContext.Provider>
+    ),
+  ],
+} as ComponentMeta<typeof LoginDialog>;
+
+const Template: ComponentStory<typeof LoginDialog> = () => <LoginDialog />;
+
+export const Default = Template.bind({});
