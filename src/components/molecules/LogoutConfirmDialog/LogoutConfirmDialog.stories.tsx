cab78158c20072421e7324d22f37e87383147d92
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-05 07:52:10 -0500

    add LogoutConfirmDialog.stories

---
+++ src/components/molecules/LogoutConfirmDialog/LogoutConfirmDialog.stories.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { action } from "@storybook/addon-actions";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { AuthContext } from "../../../contexts/AuthContext";
+import { UiStateContext } from "../../../contexts/UiStateContext";
+import { ProvidedAuth } from "../../../types/ProvidedAuth";
+import { MockActionUiStateController } from "../../__mocks__/MockActionUiStateController";
+
+import LogoutConfirmDialog from "./LogoutConfirmDialog";
+
+export default {
+  component: LogoutConfirmDialog,
+  decorators: [
+    story => (
+      <UiStateContext.Provider value={{
+        state: {
+          loginState: "LOGGING_OUT",
+        },
+        controller: MockActionUiStateController,
+      }}>
+        {story()}
+      </UiStateContext.Provider>
+    ),
+    story => (
+      <AuthContext.Provider value ={{
+        logout: () => {
+          action("logout")([]);
+          return Promise.resolve();
+        },
+      } as ProvidedAuth}>
+        {story()}
+      </AuthContext.Provider>
+    ),
+  ],  
+} as ComponentMeta<typeof LogoutConfirmDialog>;
+
+const Template: ComponentStory<typeof LogoutConfirmDialog> = () => <LogoutConfirmDialog />;
+
+export const Default = Template.bind({});
