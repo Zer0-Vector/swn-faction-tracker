0c1cf138127b45247268b4832dbe6d02c8d31e7e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-05 07:37:15 -0500

    add RegistrationDialog.stories

---
+++ src/components/molecules/RegistrationDialog/RegistrationDialog.stories.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import { action } from "@storybook/addon-actions";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { AuthContext } from "../../../contexts/AuthContext";
+import { UiStateContext } from "../../../contexts/UiStateContext";
+import { UiStateController } from "../../../controllers/UiStateController";
+import { ProvidedAuth } from "../../../types/ProvidedAuth";
+import UiState from "../../../types/UiState";
+import { MockAction } from "../../__mocks__/MockAction";
+
+import RegistrationDialog from "./RegistrationDialog";
+
+const mockAuth = {
+  signup: (...args) => {
+    action("signup")(...args);
+    return Promise.resolve({
+      emailVerified: false,
+    });
+  },
+  sendEmailVerification: () => {
+    action("sendEmailVerification")([]);
+    return Promise.resolve();
+  },
+} as ProvidedAuth;
+
+export default {
+  component: RegistrationDialog,
+  decorators: [
+    story => (
+      <AuthContext.Provider value={mockAuth}>
+        {story()}
+      </AuthContext.Provider>
+    ),
+    story => (
+      <UiStateContext.Provider value={{
+        state: {
+          loginState: "REGISTERING",
+        } as UiState,
+        controller: {
+          ...MockAction("setLoginState"),
+        } as unknown as UiStateController,
+      }}>
+        {story()}
+      </UiStateContext.Provider>
+    ),
+  ],
+} as ComponentMeta<typeof RegistrationDialog>;
+
+const Template: ComponentStory<typeof RegistrationDialog> = () => <RegistrationDialog />;
+
+export const Default = Template.bind({});
