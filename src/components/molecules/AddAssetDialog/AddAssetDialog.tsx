5e0eb7672b6795339d1d3d5cb331abeada1bbecc
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-06 22:44:47 -0500

    convert AddAssetDialog to use MessageDialog

--- src/components/molecules/AddAssetDialog/AddAssetDialog.tsx
+++ src/components/molecules/AddAssetDialog/AddAssetDialog.tsx
@@ -2,18 +2,14 @@ import React, { useCallback, useMemo, useState } from "react";
 
 import { SxProps } from "@mui/material";
 import Autocomplete, { AutocompleteRenderInputParams } from "@mui/material/Autocomplete";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
 import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
 
 import ASSETS from "../../../data/Assets";
 import Nullable from "../../../types/Nullable";
 import TextUtils from "../../../utils/TextUtils";
+import MessageDialog from "../../atoms/MessageDialog";
+import { DialogActionHandler } from "../../atoms/MessageDialog/MessageDialog";
 
 interface AddAssetDialogProps {
   open: boolean;
@@ -42,24 +38,6 @@ export default function AddAssetDialog({ open, onClose, onAdd }: AddAssetDialogP
       })
   ), []);
 
-  const handleClose = useCallback(() => {
-    setSelection("");
-    onClose();
-  }, [onClose]);
-
-  const handleCancel = useCallback((evt: React.MouseEvent<HTMLElement>) => {
-    evt.stopPropagation();
-    handleClose();
-  }, [handleClose]);
-
-  const handleAdd = useCallback((evt: React.FormEvent<HTMLElement>) => {
-    evt.stopPropagation();
-    if (selection !== "") {
-      onAdd(selection);
-      handleClose();
-    }
-  }, [handleClose, onAdd, selection]);
-
   const handleSelectionChanged = useCallback((_evt: React.SyntheticEvent, value: Nullable<AssetOption>) => {
     if (value === null) {
       setSelection("");
@@ -68,6 +46,11 @@ export default function AddAssetDialog({ open, onClose, onAdd }: AddAssetDialogP
     }
   }, []);
 
+  const handleClose = useCallback(() => {
+    setSelection("");
+    onClose();
+  }, [onClose]);
+
   const formControlSx = useMemo<SxProps>(() => ({ my: 1, minWidth: 200 }), []);
   const autoGroupBy = useCallback((o: AssetOption): string => o.group, []);
   const autoOptLabel = useCallback((o: AssetOption): string => o.name, []);
@@ -75,29 +58,35 @@ export default function AddAssetDialog({ open, onClose, onAdd }: AddAssetDialogP
   // ? extract TextField to custom component with React.memo?
   const autoRenderInput = useCallback((params: AutocompleteRenderInputParams) => <TextField {...params} label="Select Asset" data-testid="selection-field" />, []);
   
+  const handleAction = useCallback<DialogActionHandler>((_, reason) => {
+    handleClose();
+    if (reason === "Add" && selection !== "") {
+      onAdd(selection);
+    }
+  }, [handleClose, onAdd, selection]);
+
   return (
-    <Dialog open={open} onClose={handleClose} data-testid="add-asset-dialog">
-      <DialogTitle data-testid="title">Add Asset</DialogTitle>
-      <DialogContent data-testid="content">
-        <DialogContentText data-testid="content-text">Select an asset to add.</DialogContentText>
-        <FormControl sx={formControlSx}>
-          <Autocomplete
-            id="asset-select-field"
-            options={options}
-            groupBy={autoGroupBy}
-            getOptionLabel={autoOptLabel}
-            isOptionEqualToValue={optionsAreEqual}
-            disableClearable={true}
-            onChange={handleSelectionChanged}
-            renderInput={autoRenderInput}
-            data-testid="asset-autocomplete"
-          />
-        </FormControl>
-      </DialogContent>
-      <DialogActions data-testid="actions">
-        <Button onClick={handleCancel} data-testid="cancel-button">Cancel</Button>
-        <Button onClick={handleAdd} disabled={selection === ""} data-testid="add-button">Add</Button>
-      </DialogActions>
-    </Dialog>
+    <MessageDialog
+      open={open}
+      title="Add Asset"
+      message="Select an asset to add."
+      buttons={["Cancel", "Add"]}
+      onAction={handleAction}
+      data-testid="add-asset-dialog"
+    >
+      <FormControl sx={formControlSx}>
+        <Autocomplete
+          id="asset-select-field"
+          options={options}
+          groupBy={autoGroupBy}
+          getOptionLabel={autoOptLabel}
+          isOptionEqualToValue={optionsAreEqual}
+          disableClearable={true}
+          onChange={handleSelectionChanged}
+          renderInput={autoRenderInput}
+          data-testid="asset-autocomplete"
+        />
+      </FormControl>
+    </MessageDialog>
   );
 }
