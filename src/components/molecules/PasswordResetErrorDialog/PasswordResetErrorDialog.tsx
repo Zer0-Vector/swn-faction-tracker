bca3f88ed186a4e97ef1f0e8150f49fec1fdcc4e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-07 09:02:28 -0500

    add PasswordResetErrorDialog

---
+++ src/components/molecules/PasswordResetErrorDialog/PasswordResetErrorDialog.tsx
@@ -0,0 +1,24 @@
+import React, { useCallback, useContext, useMemo } from "react";
+
+import { UiStateContext } from "../../../contexts/UiStateContext";
+import MessageDialog from "../../atoms/MessageDialog";
+import { DialogActionHandler } from "../../atoms/MessageDialog/MessageDialog";
+
+export function PasswordResetErrorDialog() {
+  const { state, controller } = useContext(UiStateContext);
+  const buttons = useMemo(() => ["OK"], []);
+  const handleAction = useCallback<DialogActionHandler>(() => {
+    controller.setLoginState("LOGGING_IN");
+  }, [controller]);
+  
+  return (
+    <MessageDialog
+      open={state.loginState === "PASSWORD_RESET_ERROR"}
+      title="Error"
+      message="An error ocurred sending password reset email. Try again later."
+      onAction={handleAction}
+      buttons={buttons}
+      data-testid="reset-error-dialog"
+    />
+  );
+}
