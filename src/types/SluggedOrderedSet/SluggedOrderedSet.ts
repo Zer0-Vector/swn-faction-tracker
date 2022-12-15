7237b98118a61105aeb71a5c3da85d004a9f7988
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-15 17:20:29 -0500

    extract interface from SluggestOrderedSet

--- src/types/SluggedOrderedSet/SluggedOrderedSet.ts
+++ src/types/SluggedOrderedSet/SluggedOrderedSet.ts
@@ -1,11 +1,84 @@
 import { Maybe } from "../Maybe";
 import { SluggedEntity } from "../SluggedEntity";
 
+export interface ISluggedOrderedSet<T extends SluggedEntity> {
+  /**
+   * Adds an element to the tail of the set.
+   * @param element The element to add having properties "id" and "slug".
+   * @throws If given id is blank or it conflicts with an existing id.
+   * @throws If given slug is blank or it conflicts with an existing slug.
+   */
+  add(element: T): void;
+
+  /**
+   * Removes an element from the set.
+   * @param id The id of the element to remove.
+   * @returns `true` if the element exists and was removed. `false`, otherwise.
+   */
+  remove(id: string): boolean;
+  
+  /**
+   * Renames a slug for a specific element. The new slug is trimmed before adding.
+   * @param updateInfo An object with the id of the target element and the new slug for said element.
+   * @throws If the given id does not exist in the set.
+   * @throws If new slug is blank or conflicts with an existing element.
+   */
+  renameSlug(info: SluggedEntity): void;
+  
+  /**
+   * Retrieves an element from the set by its `id`.
+   * @param id The id of the element to retrieve.
+   * @returns The element with the given `id`, or `undefined` if the id does not map to anything.
+   */
+  get(id: string): Maybe<T>;
+
+  /**
+   * Retrieves an element from the set by its `slug`.
+   * @param slug The slug of the element to retrieve.
+   * @returns The element with the given `slug`, or `undefined` if the slug does not map to anything.
+   */
+  slugGet(slug: string): Maybe<T>;
+
+  /**
+   * Retrieves all elements in the set in order.
+   * @returns An array of the elements in this set in order.
+   */
+  getAll(): T[];
+
+  /**
+   * Moves an element from its current index to a target index.
+   * @param source The index of the element to move.
+   * @param target The destination index for the element.
+   */
+  reorder(source: number, target: number): void;
+
+  /**
+   * Checks if an element in the set has a given id.
+   * @param id The id of an element.
+   * @returns `true` if an element in the set has the given id. `false` otherwise.
+   */
+  hasId(id: string): boolean;
+
+  /**
+   * Checks if an element in the set has the given slug.
+   * @param slug The slug of an element.
+   * @returns `true` if an element in the set has the given slug. `false` otherwise.
+   */
+  hasSlug(slug: string): boolean;
+
+  /**
+   * Checks if the given entity has a unique id and a unique slug with respect to the existing elements in the set.
+   * @param entity A potential element to add to the set.
+   * @returns `true` if the element has a unique id among existing element ids and a unique slug among existing element slugs. `false` otherwise.
+   */
+  noConflicts(entity: SluggedEntity): boolean;
+}
+
 /**
  * An ordered set with elements having two unique identifiers: `id` and `slug`.
  * The ordering is based on the order elements are added, however, elements can be reordered.
  */
-export class SluggedOrderedSet<T extends SluggedEntity> {
+export class SluggedOrderedSet<T extends SluggedEntity> implements ISluggedOrderedSet<T> {
   
   private id2element: Map<string, T>;
   private slug2id: Map<string, string>;
@@ -23,12 +96,6 @@ export class SluggedOrderedSet<T extends SluggedEntity> {
     initialValues.forEach(value => this.add(value));
   }
 
-  /**
-   * Adds an element to the tail of the set.
-   * @param element The element to add having properties "id" and "slug".
-   * @throws If given id is blank or it conflicts with an existing id.
-   * @throws If given slug is blank or it conflicts with an existing slug.
-   */
   add(element: T): void {
     const { id, slug } = element;
     if (this.id2element.has(id)) {
@@ -48,11 +115,6 @@ export class SluggedOrderedSet<T extends SluggedEntity> {
     this.order.push(id);
   }
 
-  /**
-   * Removes an element from the set.
-   * @param id The id of the element to remove.
-   * @returns `true` if the element exists and was removed. `false`, otherwise.
-   */
   remove(id: string): boolean {
     const element = this.id2element.get(id);
     if (element === undefined) {
@@ -67,12 +129,6 @@ export class SluggedOrderedSet<T extends SluggedEntity> {
     return result;
   }
 
-  /**
-   * Renames a slug for a specific element. The new slug is trimmed before adding.
-   * @param updateInfo An object with the id of the target element and the new slug for said element.
-   * @throws If the given id does not exist in the set.
-   * @throws If new slug is blank or conflicts with an existing element.
-   */
   renameSlug(updateInfo: SluggedEntity): void {
     const storedElement = this.id2element.get(updateInfo.id);
     if (storedElement === undefined) {
@@ -93,20 +149,10 @@ export class SluggedOrderedSet<T extends SluggedEntity> {
     storedElement.slug = trimmed;
   }
 
-  /**
-   * Retrieves an element from the set by its `id`.
-   * @param id The id of the element to retrieve.
-   * @returns The element with the given `id`, or undefined if the id does not map to anything.
-   */
   get(id: string): Maybe<T> {
     return this.id2element.get(id);
   }
 
-  /**
-   * Retrieves an element from the set by its `slug`.
-   * @param slug The slug of the element to retrieve.
-   * @returns The element with the given `slug`, or undefined if the slug does not map to anything.
-   */
   slugGet(slug: string): Maybe<T> {
     const id = this.slug2id.get(slug);
     if (id === undefined) {
@@ -115,20 +161,11 @@ export class SluggedOrderedSet<T extends SluggedEntity> {
     return this.id2element.get(id);
   }
 
-  /**
-   * Retrieves all elements in the set in order.
-   * @returns An array of the elements in this set in order.
-   */
   getAll(): T[] {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     return this.order.map(id => this.id2element.get(id)!);
   }
 
-  /**
-   * Moves an element from its current index to a target index.
-   * @param source The index of the element to move.
-   * @param target The destination index for the element.
-   */
   reorder(source: number, target: number) {
     if (source < 0 || source > this.order.length) {
       throw new Error(`Source index out of bounds: ${source}`);
@@ -142,30 +179,15 @@ export class SluggedOrderedSet<T extends SluggedEntity> {
     this.order.splice(target, 0, removed);
   }
 
-  /**
-   * Checks if an element in the set has a given id.
-   * @param id The id of an element.
-   * @returns `true` if an element in the set has the given id. `false` otherwise.
-   */
   hasId(id: string) {
     return this.id2element.has(id);
   }
 
-  /**
-   * Checks if an element in the set has the given slug.
-   * @param slug The slug of an element.
-   * @returns `true` if an element in the set has the given slug. `false` otherwise.
-   */
   hasSlug(slug: string) {
     return this.slug2id.has(slug);
   }
 
-  /**
-   * Checks if the given entity has a unique id and a unique slug with respect to the existing elements in the set.
-   * @param entity A potential element to add to the set.
-   * @returns `true` if the element has a unique id among existing element ids and a unique slug among existing element slugs. `false` otherwise.
-   */
-  noConflicts(entity: T) {
+  noConflicts(entity: SluggedEntity) {
     return !this.hasId(entity.id) && !this.hasSlug(entity.slug);
   }
 
