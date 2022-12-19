225fb55ceafe390134a1f94d71cddfb93993689b
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-19 09:45:02 -0500

    remove AssetId class; replace with string id

--- src/types/PurchasedAsset.ts
+++ src/types/PurchasedAsset.ts
@@ -1,16 +1,15 @@
-import AssetId from "./AssetId";
+import Nullable from "./Nullable";
 
 export default class PurchasedAsset {
   
   constructor(
-    public readonly id: AssetId,
-    public hp: number,
+    public readonly id: string,
+    public slug: string,
+    public readonly name: string,
+    public readonly factionId: string,
+    public hp: Nullable<number>,
     public nickname?: string,
     public locationId?: string
   ) {}
-
-  static getKey(factionName: string, pa: PurchasedAsset): string {
-    return `${factionName}.${AssetId.toRefFormat(pa.id)}`;
-  }
 
 }
