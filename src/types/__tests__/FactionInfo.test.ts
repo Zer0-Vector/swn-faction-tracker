857b41e09ab1ce6487364a4b90f3f2fdaac13d18
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-19 14:23:09 -0500

    fix FactionInfo.test

--- src/types/__tests__/FactionInfo.test.ts
+++ src/types/__tests__/FactionInfo.test.ts
@@ -1,7 +1,7 @@
 import FactionInfo from "../FactionInfo";
 
 it('Recompute HP method sets maxhp', () => {
-  const info = new FactionInfo("test", "test");
+  const info = new FactionInfo("123", "test", "test");
   expect(info.maxHp).toBe(4);
   info.cunning += 1;
   FactionInfo.recomputeMaxHp(info);
