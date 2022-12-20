04ffe35f631e494e7aa5b5ffc6f7f0bad8a7b7a9
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-20 08:48:40 -0500

    stubbed in AssetList test

--- src/components/organisms/AssetList/AssetList.test.tsx
+++ src/components/organisms/AssetList/AssetList.test.tsx
@@ -1,5 +1,50 @@
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
+
+import { render, screen } from "@testing-library/react";
+
+import { AssetContext, AssetPoset } from "../../../contexts/AssetContext";
+import { FactionContext, FactionPoset } from "../../../contexts/FactionContext";
+import PurchasedAsset from "../../../types/PurchasedAsset";
+
+import AssetList from "./AssetList";
+
+function renderIt(assets: PurchasedAsset[], route = "/factions/test-faction") {
+  render(
+    <MemoryRouter initialEntries={[route]}>
+      <FactionContext.Provider value={{
+        factions: {
+          slugGet: (factionSlug) => ({
+            id: "123",
+            slug: factionSlug,
+            name: factionSlug.replaceAll(/-/g, " "),
+          }),
+        } as FactionPoset,
+      }}>
+        <AssetContext.Provider value={{
+          assets: {
+            getAll: () => assets,
+          } as AssetPoset,
+        }}>
+          <AssetList />
+        </AssetContext.Provider>
+      </FactionContext.Provider>
+    </MemoryRouter>
+  );
+}
 
 describe('AssetList', () => {
-  it.todo('renders');
+  it('renders with no assets', () => {
+    renderIt([]);
+    expect(screen.getByText("No Assets")).toBeInTheDocument();
+  });
+
+  it('renders with no faction', () => {
+    renderIt([], "/factions");
+    expect(screen.getByText("No faction selected!")).toBeInTheDocument();
+  });
+
+  it.todo('renders with one asset');
+  it.todo('renders with three assets');
+  it.todo('renders expanded when asset is selected');
 });
