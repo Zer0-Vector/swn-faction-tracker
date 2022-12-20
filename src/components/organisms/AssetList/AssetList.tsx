788ca808bf0f01199d455653d77f5328fe7f20ad
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-20 17:38:48 -0500

    update AssetList with subscriber hook

--- src/components/organisms/AssetList/AssetList.tsx
+++ src/components/organisms/AssetList/AssetList.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Accordion from "@mui/material/Accordion";
@@ -10,6 +10,7 @@ import { AssetContext } from "../../../contexts/AssetContext";
 import { useSelectedFaction } from "../../../hooks/useSelectedFaction";
 import { useSelectionSlug } from "../../../hooks/useSelectionSlug";
 import { RequiredChildrenProps } from "../../../types/ChildrenProps";
+import { Maybe } from "../../../types/Maybe";
 import PurchasedAsset from "../../../types/PurchasedAsset";
 import AssetDetails from "../AssetDetails";
 
@@ -28,11 +29,22 @@ const AssetSummaryComponent = ({ expanded, children }: AssetSummaryProps) => {
 
 const AssetSummary = React.memo(AssetSummaryComponent, (prev, next) => (prev.expanded === next.expanded && prev.children === next.children));
 
+function useAssetList(factionId: Maybe<string>) {
+  const { assets } = useContext(AssetContext);
+  const [list, setList] = useState(assets.getAll().filter(a => a.factionId === factionId));
+  useEffect(() => assets.subscribe(
+    () => setList(assets.getAll().filter(a => a.factionId === factionId))
+  ), [assets, factionId]);
+  return {
+    list,
+  };
+}
+
 export default function AssetList() {
-  const { assets: assetSet } = useContext(AssetContext);
+  const faction = useSelectedFaction();
+  const { list: assets } = useAssetList(faction?.id);
   const { assetSlug, factionSlug } = useSelectionSlug();
   const nav = useNavigate();
-  const faction = useSelectedFaction();
 
   
   const handleSelectAsset = (pa: PurchasedAsset) => (_evt: React.SyntheticEvent, expanded: boolean) => {
@@ -48,7 +60,6 @@ export default function AssetList() {
     return <Typography color="error">No faction selected!</Typography>;
   }
   
-  const assets: PurchasedAsset[] = assetSet.getAll().filter(a => a.factionId === faction.id);
   console.log("Rendering asset list: ", assets);
   return (
     // TODO make drag and drop
