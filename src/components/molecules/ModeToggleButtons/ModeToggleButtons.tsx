754f0287f3a3b20fe836aa78baddae233157c857
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2022-12-26 14:41:03 -0500

    add confirmation for ending turn

--- src/components/molecules/ModeToggleButtons/ModeToggleButtons.tsx
+++ src/components/molecules/ModeToggleButtons/ModeToggleButtons.tsx
@@ -1,18 +1,39 @@
-import React, { useContext } from "react";
+import React, { useCallback, useContext, useState } from "react";
 
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
 import { UiStateContext } from "../../../contexts/UiStateContext";
+import { useConfirmation } from "../../../hooks/useConfirmation";
+import { isGameMode } from "../../../types/GameMode";
+import Nullable from "../../../types/Nullable";
 
 export default function ModeToggleButtons() {
   const { state, controller } = useContext(UiStateContext);
+  const confirm = useConfirmation();
 
-  const handleChange = (_: React.MouseEvent, value: string) => {
-    if (value) {
-      controller.setEditMode(value);
+  const handleChange = useCallback((_: React.MouseEvent, value: Nullable<string>) => {
+    if (value === null) { // when active button is clicked
+      return;
     }
-  };
+
+    if (isGameMode(value)) {
+      if (state.editMode === "TURN" && value !== "TURN") {
+        confirm({
+          title: "End Turn Confirmation",
+          message: "The turn is incomplete. Do you really want to end the turn?",
+        }).then((confirmed: boolean) => {
+          if (confirmed) {
+            controller.setEditMode(value, true);
+          }
+        });
+      } else {
+        controller.setEditMode(value);
+      }
+    } else {
+      console.warn("Unknown ModeToggleButton value: ", value);
+    }
+  }, [confirm, controller, state.editMode]);
 
   console.debug("Rendering ModeToggleButtons...");
 
