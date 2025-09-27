f24000ba331f4d6ef184f86193bdc11755d7fd9a
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-09-27 13:33:45 -0400

    utilize useX hooks and rename some shit

--- src/contexts/providers/LocationContextProvider.tsx
+++ src/contexts/providers/LocationContextProvider.tsx
@@ -3,9 +3,10 @@ import React, { useEffect, useMemo, useRef } from "react";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import LocationInfo from "../../types/LocationInfo";
 import { NamedElementPoset } from "../../types/NamedElementPoset";
+import { ReadonlyPropsWithChildren } from "../../types/ReadonlyPropsWithChildren";
 import { LocationContext } from "../LocationContext";
 
-export function LocationContextProvider({ children }: React.PropsWithChildren<{}>) {
+export function LocationContextProvider({ children }: ReadonlyPropsWithChildren) {
   const [storedLocations, setStoredLocations] = useLocalStorage<LocationInfo[]>("swn-faction-tracker.locations", []);
   const locations = useRef(new NamedElementPoset<LocationInfo, { tl: number, x: number, y: number }>(
     info => new LocationInfo(info.id, info.slug, info.name, info.tl, info.x, info.y),
@@ -16,10 +17,9 @@ export function LocationContextProvider({ children }: React.PropsWithChildren<{}
     () => {
       setStoredLocations(locations.current.getAll());
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  ), []);
+  ), [setStoredLocations]);
 
-  const context = useMemo(() => ({ locations: locations.current }), []);
+  const context = useMemo(() => ({ locations: locations.current }), [locations]);
 
   return (
     <LocationContext.Provider value={context}>
