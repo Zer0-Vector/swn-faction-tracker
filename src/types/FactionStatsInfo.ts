5967286ce152e53e8cebce347cc7fa9b4f71ca56
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-09-27 12:31:09 -0400

    simplify FactionStatsInfo definition

--- src/types/FactionStatsInfo.ts
+++ src/types/FactionStatsInfo.ts
@@ -1,13 +1,10 @@
-export type FactionStat = 
-  | "hp"
-  | "maxHp"
-  | "force"
-  | "cunning"
-  | "wealth"
-  | "xp";
-
-type FactionStatsInfo = {
-  [Stat in FactionStat]: number
-};
+interface FactionStatsInfo {
+  hp: number;
+  readonly maxHp: number;
+  force: number;
+  cunning: number;
+  wealth: number;
+  xp: number;
+}
 
 export default FactionStatsInfo;
