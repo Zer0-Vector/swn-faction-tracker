9f9e9065ef677c076628afd830f83237a3586c5a
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-09-27 12:34:14 -0400

    TurnPhase interface and helpers

---
+++ src/types/TurnPhase.ts
@@ -0,0 +1,23 @@
+import { Maybe } from "./Maybe";
+
+export const TurnPhaseNames = [
+  "Select Faction",
+  "Set Goal",
+  "Select Action",
+] as const;
+
+export type TurnPhaseName = typeof TurnPhaseNames[number];
+
+export function isTurnStepName(s: string): s is TurnPhaseName {
+  return TurnPhaseNames.includes(s as TurnPhaseName);
+}
+
+export type TurnPhaseNameValue = TurnPhaseName | "";
+
+export interface TurnPhase {
+  readonly name: TurnPhaseNameValue;
+  readonly icon?: JSX.Element;
+  readonly title?: string;
+  readonly onClick: Maybe<()=>void>;
+  readonly onExit?: ()=>void;
+}
