27f9867ca3f8a894def2ce930353747884c74258
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-09-29 11:07:18 -0400

    configure vite .env variables

--- src/firebase-init.ts
+++ src/firebase-init.ts
@@ -6,7 +6,8 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
+const firebaseConfigString = import.meta.env.VITE_FIREBASE_CONFIG;
+const firebaseConfig = JSON.parse(firebaseConfigString);
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
