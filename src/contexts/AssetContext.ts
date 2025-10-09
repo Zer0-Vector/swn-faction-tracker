35c9470eaceb61c5ddd0e6522d62065c0d7244e0
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-10-09 09:15:38 -0400

    refactor AssetContext & provider... use AssetPoset and useSyncExternalStore

--- src/contexts/AssetContext.ts
+++ src/contexts/AssetContext.ts
@@ -1,12 +1,30 @@
 import React from "react";
 
-import { INamedElementPoset } from "../types/NamedElementPoset";
-import PurchasedAsset from "../types/PurchasedAsset";
+import { NamedElementPoset } from "../utils/NamedElementPoset";
+import PurchasedAsset from "../utils/PurchasedAsset";
 
-export type AssetPoset = INamedElementPoset<PurchasedAsset, { factionId: string }, "name">;
+export class AssetPoset extends NamedElementPoset<PurchasedAsset, { factionId: string }, "name", "factionId"> {
+  constructor(elements: PurchasedAsset[] = []) {
+    super(PurchasedAsset.from, elements,
+      (args: { factionId: string }) =>
+          (e: PurchasedAsset) => {
+            return e.factionId === args.factionId;
+          }
+    );
+  }
+}
 
 export interface AssetContextType {
   assets: AssetPoset;
 }
 
 export const AssetContext = React.createContext<AssetContextType>({} as AssetContextType);
+
+export const useAssets = () => {
+  const ctx = React.useContext(AssetContext);
+  if (!ctx) throw new Error("useAssets must be used within a AssetContextProvider");
+
+  return React.useSyncExternalStore(
+      ctx.assets.subscribe.bind(ctx.assets),
+      () => ctx.assets);
+}
