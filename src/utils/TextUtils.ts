55fb2c27429f5d633c6470b18415d40627b2cc98
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-10-13 17:10:10 -0400

    convert utility class to object

--- src/utils/TextUtils.ts
+++ src/utils/TextUtils.ts
@@ -1,15 +1,16 @@
-export default class TextUtils {
-  
-  static capitalize([first, ...rest]: string, locale = navigator.language) {
-    return !first ? '' : first.toLocaleUpperCase(locale) + rest.join('');
-  }
+const TextUtils = {
 
-  static titleCase(input: string, locale = navigator.language) {
+  capitalize([first, ...rest]: string, locale = navigator.language) {
+    return !first ? '' : first.toLocaleUpperCase(locale) + rest.join('');
+  },
+
+  titleCase(input: string, locale = navigator.language) {
     const words = input.split(/\s/g);
     return words
       .map(word => word.toLocaleLowerCase(locale))
       .map(word => TextUtils.capitalize(word, locale))
       .join(" ");
   }
+};
 
-}
+export default TextUtils;
