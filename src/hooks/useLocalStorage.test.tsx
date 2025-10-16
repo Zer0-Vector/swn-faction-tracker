8f192c64d5bf07bde4cd9f2bc54a7857d753146e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-10-16 08:20:10 -0400

    wrap test in describe

--- src/hooks/useLocalStorage.test.tsx
+++ src/hooks/useLocalStorage.test.tsx
@@ -24,45 +24,47 @@ function TestComponent(): JSX.Element {
   );
 }
 
-beforeEach(() => {
-  localStorage.clear();
-});
+describe("useLocalStorage", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
 
-test("useLocalStorage returns default value if unset", () => {
-  render(<TestComponent />);
-  const value = screen.getByTestId("test-value").textContent;
-  expect(value).toBe("default test value 123");
-});
+  test("useLocalStorage returns default value if unset", () => {
+    render(<TestComponent />);
+    const value = screen.getByTestId("test-value").textContent;
+    expect(value).toBe("default test value 123");
+  });
 
-test("useLocalStorage setValue changes output", () => {
-  render(<TestComponent />);
-  const value1 = screen.getByTestId("test-value").textContent;
-  expect(value1).toBe("default test value 123");
+  test("useLocalStorage setValue changes output", () => {
+    render(<TestComponent />);
+    const value1 = screen.getByTestId("test-value").textContent;
+    expect(value1).toBe("default test value 123");
 
-  const button = screen.getByTestId("test-button");
-  expect(button).toBeDefined();
-  button.click();
+    const button = screen.getByTestId("test-button");
+    expect(button).toBeDefined();
+    button.click();
 
-  const value = screen.getByTestId("test-value").textContent;
-  expect(value).toBe("new test value 4567");
-});
+    const value = screen.getByTestId("test-value").textContent;
+    expect(value).toBe("new test value 4567");
+  });
 
-test("rerender does not change data", () => {
-  const { unmount } = render(<TestComponent />);
-  const testElement = screen.getByTestId("test-value");
-  const value1 = testElement.textContent;
-  expect(value1).toBe("default test value 123");
+  test("rerender does not change data", () => {
+    const { unmount } = render(<TestComponent />);
+    const testElement = screen.getByTestId("test-value");
+    const value1 = testElement.textContent;
+    expect(value1).toBe("default test value 123");
 
-  const button = screen.getByTestId("test-button");
-  expect(button).toBeDefined();
-  button.click();
+    const button = screen.getByTestId("test-button");
+    expect(button).toBeDefined();
+    button.click();
 
-  const value = testElement.textContent;
-  expect(value).toBe("new test value 4567");
-  unmount();
-  expect(testElement).not.toBeInTheDocument();
+    const value = testElement.textContent;
+    expect(value).toBe("new test value 4567");
+    unmount();
+    expect(testElement).not.toBeInTheDocument();
 
-  render(<TestComponent />);
-  const value2 = testElement.textContent;
-  expect(value2).toBe("new test value 4567");
+    render(<TestComponent />);
+    const value2 = testElement.textContent;
+    expect(value2).toBe("new test value 4567");
+  });
 });
