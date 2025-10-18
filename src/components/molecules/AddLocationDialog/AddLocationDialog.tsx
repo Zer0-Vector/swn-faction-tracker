57040f82aafea2f4a3f7a7ae00455157c0e16d0e
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-10-18 14:59:17 -0400

    remove extracted logic and use subcomponents

--- src/components/molecules/AddLocationDialog/AddLocationDialog.tsx
+++ src/components/molecules/AddLocationDialog/AddLocationDialog.tsx
@@ -1,13 +1,13 @@
-import React, { useCallback, useMemo, useRef, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 
+import MessageDialog, { DialogActionHandler } from "@/components/atoms/MessageDialog";
 import Box from "@mui/material/Box";
-import MenuItem from "@mui/material/MenuItem";
-import TextField from "@mui/material/TextField";
 
-import { useLocations } from "../../../contexts/LocationContext";
-import FormInfo from "../../../types/FormInfo";
-import MessageDialog from "../../atoms/MessageDialog";
-import { DialogActionHandler } from "../../atoms/MessageDialog/MessageDialog";
+import NameTextField from "./fields/NameTextField";
+import TechLevelTextField from "./fields/TechLevelTextField";
+import { LocationDialogFieldHandles } from "./fields/utils";
+import XCoordTextField from "./fields/XCoordTextField";
+import YCoordTextField from "./fields/YCoordTextField";
 
 interface AddLocationDialogProps {
   readonly open: boolean;
@@ -20,109 +20,50 @@ interface AddLocationDialogProps {
   }) => void;
 }
 
-type Coordinate<T> = [x: T, y: T];
-
-const BLANK_FORM_INFO: FormInfo = { value: "", valid: false };
-const BLANK_COORDS: FormInfo<Coordinate<string>> = {
-  value: ["", ""],
-  valid: false,
-};
-
-type FormInfoSetter = (val: FormInfo) => void;
-type StringValidator = (val: string) => boolean;
-
 // FIXME refactor into sub-components
 export default function AddLocationDialog({
   open,
   onClose,
   onCreate,
 }: AddLocationDialogProps) {
-  const locations = useLocations();
-  const [nameText, setNameText] = useState<FormInfo>(BLANK_FORM_INFO);
-  const [tlText, setTlText] = useState<FormInfo>(BLANK_FORM_INFO);
-  const [coords, setCoords] =
-    useState<FormInfo<Coordinate<string>>>(BLANK_COORDS);
-  const nameRef = useRef<HTMLInputElement>(null);
 
-  const handleChange =
-    (setter: FormInfoSetter, valid?: StringValidator) =>
-    (evt: React.ChangeEvent<HTMLInputElement>) => {
-      const newText = evt.target.value;
-      const isValid = valid === undefined || valid(newText);
-      const isNotBlank = newText !== undefined && newText.trim().length > 0;
-      const newState = {
-        value: newText,
-        valid: isNotBlank && isValid,
-      };
-      setter(newState);
-    };
+  console.log("Rendering AddLocationDialog...");
 
-  const handleCoordsChange =
-    (index: number) => (evt: React.ChangeEvent<HTMLInputElement>) => {
-      if (index > 1) {
-        throw new Error("wtf...index is out of range");
-      }
+  const nameRef = useRef<LocationDialogFieldHandles>(null);
+  const tlRef = useRef<LocationDialogFieldHandles>(null);
+  const xRef = useRef<LocationDialogFieldHandles>(null);
+  const yRef = useRef<LocationDialogFieldHandles>(null);
 
-      const newText = evt.target.value;
-      console.log(`validating index ${index}...`);
-      const newCoords: Coordinate<string> =
-        index === 0 ? [newText, coords.value[1]] : [coords.value[0], newText];
-      const isNotBlank = newText !== undefined && newText.trim().length > 0;
-      const newState = {
-        value: newCoords,
-        valid: isNotBlank,
-      };
-      console.log("setting coords: ", newState);
-      setCoords(newState);
-    };
-
-  const isNotDuplicateName = useCallback(
-    (val: string) => locations.checkName({ name: val }),
-    [locations]
-  );
-
-  const isInteger = (val: string) => {
-    try {
-      Number.parseInt(val);
-      return true;
-    } catch {
-      return false;
-    }
-  };
+  const [allValid, setAllValid] = useState<boolean>(false);
 
   const handleClose = useCallback(() => {
-    setNameText(BLANK_FORM_INFO);
-    setTlText(BLANK_FORM_INFO);
-    setCoords({ value: ["", ""], valid: false });
+    nameRef.current?.reset();
+    tlRef.current?.reset();
+    xRef.current?.reset();
+    yRef.current?.reset();
     nameRef.current?.focus();
     onClose();
   }, [onClose]);
 
-  const allValid = useCallback(() => {
-    return (
-      isNotDuplicateName(nameText.value)
-      && nameText.valid
-      && tlText.valid
-      && coords.valid
-    );
-  }, [
-    coords.valid,
-    isNotDuplicateName,
-    nameText.valid,
-    nameText.value,
-    tlText.valid,
-  ]);
+  const validityChanged = () => {
+    setAllValid(!!(nameRef.current?.isValid() &&
+      tlRef.current?.isValid() &&
+      xRef.current?.isValid() &&
+      yRef.current?.isValid()));
+  };
 
-  const handleCreate = useCallback(() => {
-    if (allValid()) {
+  /* eslint-disable @typescript-eslint/no-non-null-assertion */
+  const handleCreate = () => {
+    if (allValid) {
       onCreate({
-        name: nameText.value,
-        tl: Number.parseInt(tlText.value),
-        x: Number.parseInt(coords.value[0]),
-        y: Number.parseInt(coords.value[1]),
+        name: nameRef.current!.getValue(),
+        tl: Number.parseInt(tlRef.current!.getValue()),
+        x: Number.parseInt(xRef.current!.getValue()),
+        y: Number.parseInt(yRef.current!.getValue()),
       });
     }
-  }, [allValid, coords.value, nameText.value, onCreate, tlText.value]);
+  };
+  /* eslint-enable @typescript-eslint/no-non-null-assertion */
 
   const handleAction = useCallback<DialogActionHandler>(
     (result) => {
@@ -134,11 +75,8 @@ export default function AddLocationDialog({
     [handleClose, handleCreate]
   );
 
-  const buttons = useMemo(() => ["Cancel", "Create"], []);
-  const disabledButtons = useMemo(
-    () => (allValid() ? [] : ["Create"]),
-    [allValid]
-  );
+  const buttons = ["Cancel", "Create"];
+  const disabledButtons = allValid ? [] : ["Create"];
 
   return (
     <MessageDialog
@@ -151,70 +89,11 @@ export default function AddLocationDialog({
       data-testid="add-location-dialog"
     >
       <Box display="flex" flexDirection="column" gap={2}>
-        <TextField
-          id="location-name"
-          label="Location Name"
-          variant="filled"
-          inputRef={nameRef}
-          type="text"
-          placeholder="Enter Location Name"
-          autoFocus={true}
-          value={nameText.value}
-          onInput={handleChange(setNameText, isNotDuplicateName)}
-          error={!nameText.valid}
-          fullWidth={true}
-          autoComplete="off"
-          data-testid="location-name-field"
-        />
-        <TextField
-          id="location-tl"
-          label="Tech Level"
-          variant="filled"
-          autoFocus={false}
-          value={tlText.value}
-          onChange={handleChange(setTlText, isInteger)}
-          error={!tlText.valid}
-          fullWidth={true}
-          autoComplete="off"
-          select={true}
-          data-testid="location-tl-field"
-        >
-          <MenuItem value="0">0</MenuItem>
-          <MenuItem value="1">1</MenuItem>
-          <MenuItem value="2">2</MenuItem>
-          <MenuItem value="3">3</MenuItem>
-          <MenuItem value="4">4</MenuItem>
-          <MenuItem value="5">5</MenuItem>
-        </TextField>
+        <NameTextField ref={nameRef} onValidityChange={validityChanged} />
+        <TechLevelTextField ref={tlRef} onValidityChange={validityChanged} />
         <Box display="flex" justifyContent="space-between" gap={2}>
-          <TextField
-            id="location-x"
-            label="X"
-            variant="filled"
-            type="number"
-            placeholder="X Coordinate"
-            autoFocus={false}
-            value={coords.value[0]}
-            onInput={handleCoordsChange(0)}
-            error={!coords.valid}
-            autoComplete="off"
-            fullWidth={true}
-            data-testid="location-x-field"
-          />
-          <TextField
-            id="location-y"
-            label="Y"
-            variant="filled"
-            type="number"
-            placeholder="Y Coordinate"
-            autoFocus={false}
-            value={coords.value[1]}
-            onInput={handleCoordsChange(1)}
-            error={!coords.valid}
-            autoComplete="off"
-            fullWidth={true}
-            data-testid="location-y-field"
-          />
+          <XCoordTextField ref={xRef} onValidityChange={validityChanged} />
+          <YCoordTextField ref={yRef} onValidityChange={validityChanged} />
         </Box>
       </Box>
     </MessageDialog>
