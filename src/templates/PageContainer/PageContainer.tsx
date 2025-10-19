396f33bb1543cb06daede484c73877e02528c7b4
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-10-19 08:52:16 -0400

    refactor(PageContainer): responsive layout and remove useMemo
    useMemo wasn't doing anything useful here.

--- src/templates/PageContainer/PageContainer.tsx
+++ src/templates/PageContainer/PageContainer.tsx
@@ -1,40 +1,36 @@
 import React, { useContext, useMemo } from "react";
 import { Link, useLocation } from "react-router-dom";
 
+import { TurnPhaseStepper } from "@/components/atoms/TurnPhaseStepper";
+import ModeToggleButtons from "@/components/molecules/ModeToggleButtons";
+import UserMenu from "@/components/molecules/UserMenu";
+import { UiStateContext } from "@/contexts/UiStateContext";
+import { useAuth } from "@/hooks/useAuth";
+import { RequiredChildrenProps } from "@/types/ChildrenProps";
 import { ReadonlyPropsWithChildren } from "@/types/ReadonlyPropsWithChildren";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
-import Grid, { GridSize } from "@mui/material/Grid";
+import Grid, { GridTypeMap } from "@mui/material/Grid";
+import { OverrideProps } from "@mui/material/OverridableComponent";
 import Slide from "@mui/material/Slide";
-import { SxProps, Theme } from "@mui/material/styles";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 
-import { TurnPhaseStepper } from "../../components/atoms/TurnPhaseStepper";
-import ModeToggleButtons from "../../components/molecules/ModeToggleButtons";
-import UserMenu from "../../components/molecules/UserMenu";
-import { UiStateContext } from "../../contexts/UiStateContext";
-import { useAuth } from "../../hooks/useAuth";
-import { RequiredChildrenProps } from "../../types/ChildrenProps";
+type GridItemProps = OverrideProps<GridTypeMap, "div"> & RequiredChildrenProps;
 
-export interface PageContainerProps {
-  children?: React.ReactNode;
-}
-
-type GridItemProps = { xs: GridSize } & RequiredChildrenProps;
-
-const GridItemComp = ({ xs, children }: GridItemProps) => (
+const GridItemComp = (props: GridItemProps) => (
   <Grid
-    size={xs}
     display="flex"
-    justifyContent="flex-start"
+    justifyContent="center"
     alignItems="center"
+    border="red dotted 3px"
+    {...props}
   >
-    {children}
+    {props.children}
   </Grid>
 );
 
@@ -53,67 +49,58 @@ export default function PageContainer({ children }: ReadonlyPropsWithChildren) {
 
   console.debug("Rendering PageContainer...");
 
-  const tabSx = useMemo<SxProps<Theme>>(
-    () => ({
-      color: "primary.contrastText",
-      "&:hover": {
-        color: "primary.light",
-        backgroundColor: "action.hover",
-      },
-    }),
-    []
-  );
+  const tabSx = {
+    color: "primary.contrastText",
+    "&:hover": {
+      color: "primary.light",
+      backgroundColor: "action.hover",
+    },
+  };
 
-  const boxSx = useMemo<SxProps<Theme>>(
-    () => ({
-      display: "flex",
-      minHeight: "100vh",
-      flexDirection: "column",
-      alignItems: "stretch",
-    }),
-    []
-  );
+  const boxSx = {
+    display: "flex",
+    minHeight: "100vh",
+    flexDirection: "column",
+    alignItems: "stretch",
+  };
 
-  const appBarSx = useMemo<SxProps<Theme>>(
-    () => ({
-      color: "primary.contrastText",
-      backgroundColor: "primary.dark",
-    }),
-    []
-  );
+  const appBarSx = {
+    color: "primary.contrastText",
+    backgroundColor: "primary.dark",
+  };
 
-  const appTitleSx = useMemo<SxProps<Theme>>(
-    () => ({
-      mr: 2,
-      color: "primary.contrastText",
-      whiteSpace: "nowrap",
-    }),
-    []
-  );
-
-  const containerSx = useMemo<SxProps<Theme>>(
-    () => ({
-      width: "100vw",
-      display: "grid",
-      gridTemplateColumns: "40% 60%",
-    }),
-    []
-  );
+  const contentContainerSx = {
+    width: "100vw",
+    display: "grid",
+    gridTemplateColumns: "40% 60%",
+  };
 
   return (
     <>
       <CssBaseline />
       <Box sx={boxSx} data-testid="page-container">
-        <AppBar sx={appBarSx}>
-          <Toolbar>
-            <Grid container>
-              {/* TODO use different layout container */}
-              <GridItem xs={3.5}>
-                <Typography variant="h1" sx={appTitleSx}>
+        <AppBar sx={appBarSx} data-testid="app-bar">
+          <Toolbar data-testid="toolbar">
+            <Grid container
+                spacing={{ lg: 4, md: 3, sm: 2, xs: 1}}
+                columns={{ lg: 12, md: 9, xs: 4 }}
+                width="100%"
+            >
+              <GridItem
+                  size={4}
+                  justifyContent="flex-start"
+              >
+                <Typography
+                    variant="h1"
+                    textAlign="left"
+                    color="primary.contrastText"
+                    whiteSpace="nowrap"
+                    data-testid="app-title"
+                >
                   SWN Faction Tracker
                 </Typography>
               </GridItem>
-              <GridItem xs={4.5}>
+              <GridItem size={{ lg: 3, md: 5, xs: 4 }}>
                 <Tabs value={tab} component="nav">
                   <Tab
                     value="FACTIONS"
@@ -131,10 +118,10 @@ export default function PageContainer({ children }: ReadonlyPropsWithChildren) {
                   />
                 </Tabs>
               </GridItem>
-              <GridItem xs={3}>
+              <GridItem size={{ lg: 3, md: 8, xs: 3 }}>
                 <ModeToggleButtons />
               </GridItem>
-              <GridItem xs={1}>
+              <GridItem size={1}>
                 <UserMenu user={currentUser} />
               </GridItem>
             </Grid>
@@ -151,7 +138,7 @@ export default function PageContainer({ children }: ReadonlyPropsWithChildren) {
             <TurnPhaseStepper />
           </Box>
         </Slide>
-        <Container sx={containerSx}>{children}</Container>
+        <Container sx={contentContainerSx}>{children}</Container>
       </Box>
     </>
   );
