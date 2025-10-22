356a2437d22b4abcee4795e5c89bb3f434279ba3
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-10-22 09:34:51 -0400

    feat(AddAssetDialog): add selected asset's description; improved state management

--- src/components/molecules/AddAssetDialog/AddAssetDialog.tsx
+++ src/components/molecules/AddAssetDialog/AddAssetDialog.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useMemo, useState } from "react";
 
-import { SxProps } from "@mui/material";
+import { Container, SxProps, Theme, Typography } from "@mui/material";
 import Autocomplete, {
   AutocompleteRenderInputParams,
 } from "@mui/material/Autocomplete";
@@ -22,6 +22,7 @@ interface AddAssetDialogProps {
 interface AssetOption {
   name: string;
   group: string;
+  description: string;
   disabled: boolean;
 }
 
@@ -30,7 +31,7 @@ export default function AddAssetDialog({
   onClose,
   onAdd,
 }: AddAssetDialogProps) {
-  const [selection, setSelection] = useState<string>("");
+  const [selection, setSelection] = useState<Nullable<AssetOption>>(null);
 
   const options = useMemo(
     () =>
@@ -41,6 +42,7 @@ export default function AddAssetDialog({
           return {
             name: name,
             group: group,
+            description: item.description,
           } as AssetOption;
         }),
     []
@@ -48,45 +50,34 @@ export default function AddAssetDialog({
 
   const handleSelectionChanged = useCallback(
     (_evt: React.SyntheticEvent, value: Nullable<AssetOption>) => {
-      if (value === null) {
-        setSelection("");
-      } else {
-        setSelection(value.name);
-      }
+      setSelection(value);
     },
     []
   );
 
   const handleClose = useCallback(() => {
-    setSelection("");
+    setSelection(null);
     onClose();
   }, [onClose]);
 
-  const formControlSx = useMemo<SxProps>(() => ({ my: 1, minWidth: 200 }), []);
-  const autoGroupBy = useCallback((o: AssetOption): string => o.group, []);
-  const autoOptLabel = useCallback((o: AssetOption): string => o.name, []);
-  const optionsAreEqual = useCallback(
-    (o: AssetOption, v: AssetOption): boolean =>
-      o.group === v.group && o.name === v.name,
-    []
-  );
-  // ? extract TextField to custom component with React.memo?
-  const autoRenderInput = useCallback(
-    (params: AutocompleteRenderInputParams) => (
-      <TextField
-        {...params}
-        label="Select Asset"
-        data-testid="selection-field"
-      />
-    ),
-    []
+  const formControlSx: SxProps<Theme> = (theme) => ({ my: 1, minWidth: theme.breakpoints.values.xs });
+  const autoGroupBy = (o: AssetOption): string => o.group;
+  const autoOptLabel = (o: AssetOption): string => o.name;
+  const optionsAreEqual = (o: AssetOption, v: AssetOption): boolean => o.group === v.group && o.name === v.name
+  // Q: extract TextField to custom component with React.memo?
+  const autoRenderInput = (params: AutocompleteRenderInputParams) => (
+    <TextField
+      {...params}
+      label="Select Asset"
+      data-testid="selection-field"
+    />
   );
 
   const handleAction = useCallback<DialogActionHandler>(
     (result) => {
       handleClose();
-      if (result.reason === "Add" && selection !== "") {
-        onAdd(selection);
+      if (result.reason === "Add" && selection !== null) {
+        onAdd(selection.name);
       }
     },
     [handleClose, onAdd, selection]
@@ -98,23 +89,46 @@ export default function AddAssetDialog({
       title="Add Asset"
       message="Select an asset to add."
       buttons={["Cancel", "Add"]}
+      disabledButtons={selection ? undefined : ["Add"]}
       onAction={handleAction}
       data-testid="add-asset-dialog"
-      fullWidth={false}
+      fullWidth={true}
+      maxWidth="sm"
     >
-      <FormControl sx={formControlSx}>
-        <Autocomplete
-          id="asset-select-field"
-          options={options}
-          groupBy={autoGroupBy}
-          getOptionLabel={autoOptLabel}
-          isOptionEqualToValue={optionsAreEqual}
-          disableClearable={true}
-          onChange={handleSelectionChanged}
-          renderInput={autoRenderInput}
-          data-testid="asset-autocomplete"
-        />
-      </FormControl>
+      <Container sx={{
+        display: "grid",
+        gridTemplateRows: "1fr 12.5rem",
+        gridTemplateColumns: "1fr 45ch",
+        width: "100%",
+        columnGap: "1rem",
+        overflow: "hidden"
+      }} fixed>
+        <FormControl sx={formControlSx}>
+          <Autocomplete
+            id="asset-select-field"
+            options={options}
+            groupBy={autoGroupBy}
+            getOptionLabel={autoOptLabel}
+            isOptionEqualToValue={optionsAreEqual}
+            disableClearable={true}
+            onChange={handleSelectionChanged}
+            selectOnFocus
+            clearOnEscape
+            renderInput={autoRenderInput}
+            data-testid="asset-autocomplete"
+          />
+        </FormControl>
+
+
+        <Typography fontSize={28} alignSelf="center" color={selection ? undefined : "warning"}>{selection ? selection.name : "Make a selection"}</Typography>
+        <Typography
+          variant="body2"
+          overflow="scroll"
+          gridColumn="2"
+        >
+          {selection?.description}
+        </Typography>
+      </Container>
     </MessageDialog>
   );
 }
