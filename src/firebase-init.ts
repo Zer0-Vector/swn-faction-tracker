99e723dab65accb28d290c787f30954dd8c42fbb
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-10-26 14:34:28 -0400

    refactor(firebase-init): use static config; fix env var usage

--- src/firebase-init.ts
+++ src/firebase-init.ts
@@ -1,38 +1,37 @@
 // Import the functions you need from the SDKs you need
-import type { FirebaseOptions } from "firebase/app";
 import { initializeApp } from "firebase/app";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseConfig = ((): FirebaseOptions => {
-  if (import.meta.env.VITE_FIREBASE_CONFIG) {
-    console.log("Loading .env FIREBASE_CONFIG");
-    return JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
-  } else {
-    // cSpell:disable
-    return {
-      apiKey: "AIzaSyBkP-QvLft922K78h2flnbQaJgQGHJeeSM",
-      authDomain: "swn-factions.firebaseapp.com",
-      projectId: "swn-factions",
-      storageBucket: "swn-factions.firebasestorage.app",
-      messagingSenderId: "754249508647",
-      appId: "1:754249508647:web:1c97a76aa63ee8cf97eb02",
-      measurementId: "G-S5RZ10295Z",
-    };
-  }
-})();
+// cSpell:disable
+const firebaseConfig = {
+  apiKey: "AIzaSyBkP-QvLft922K78h2flnbQaJgQGHJeeSM",
+  authDomain: "swn-factions.firebaseapp.com",
+  projectId: "swn-factions",
+  storageBucket: "swn-factions.firebasestorage.app",
+  messagingSenderId: "754249508647",
+  appId: "1:754249508647:web:1c97a76aa63ee8cf97eb02",
+  measurementId: "G-S5RZ10295Z",
+};
+// cSpell:enable
 
 console.log("Configured Firebase: ", firebaseConfig.projectId);
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
 
-console.log("===> MODE: ", import.meta.env.MODE, " <===");
+console.log(`===> ${import.meta.env.MODE} mode <===`);
+
 
 if (import.meta.env.DEV) {
-  self.FIREBASE_APPCHECK_DEBUG_TOKEN = import.meta.env.VITE_FIREBASE_APPCHECK_DEBUG_TOKEN || true; // NOSONAR
+  console.log(`ReCaptcha site key from env: ${!!import.meta.env.VITE_RECAPTCHA_SITE_KEY}`);
+  const debugToken = import.meta.env.VITE_FIREBASE_APPCHECK_DEBUG_TOKEN || process.env.VITE_FIREBASE_APPCHECK_DEBUG_TOKEN;
+  console.log(`App Check debug token from env: ${!!debugToken}`);
+  self.FIREBASE_APPCHECK_DEBUG_TOKEN = debugToken || true; // NOSONAR
+} else if (import.meta.env.VITE_FIREBASE_APPCHECK_DEBUG_TOKEN || process.env.VITE_FIREBASE_APPCHECK_DEBUG_TOKEN) {
+  console.warn("App Check debug token should not be used in production!");
 }
 
 export const FirebaseAppCheck = initializeAppCheck(FirebaseApp, {
