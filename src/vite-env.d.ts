84908dbcd2e5ee6018023296bf04d3ac3b59d9e6
Zer0-Vector <77898583+Zer0-Vector@users.noreply.github.com>
2025-10-26 12:57:50 -0400

    feat: define strict type options in vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_CONFIG: string;
+  readonly VITE_RECAPTCHA_SITE_KEY: string;
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_FIREBASE_APPCHECK_DEBUG_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
